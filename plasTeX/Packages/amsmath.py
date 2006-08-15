c2c0d7fc071df354a27df412ff8b62bd9f8969ae
kesmit <kesmit>
2006-08-15 13:59:28 +0000

    Added support for some amsmath environments

--- plasTeX/Packages/amsmath.py
+++ plasTeX/Packages/amsmath.py
@@ -2,6 +2,16 @@
 
 from plasTeX import Command, Environment
 from plasTeX.Base.LaTeX.Arrays import Array
+from plasTeX.Base.LaTeX.Math import EqnarrayStar, equation
 
 class pmatrix(Array):
     pass
+
+class AlignStar(EqnarrayStar):
+    """ AMSMath """
+    macroName = 'align*'
+
+class align(equation):
+    """ AMSMath """
+    pass
+
