bb9571545d2a849cace594b9f45126d74d48d811
kesmit <kesmit>
2006-11-27 18:42:22 +0000

    Update amsmath

--- plasTeX/Packages/amsmath.py
+++ plasTeX/Packages/amsmath.py
@@ -7,11 +7,42 @@ from plasTeX.Base.LaTeX.Math import EqnarrayStar, equation, eqnarray
 class pmatrix(Array):
     pass
 
-class AlignStar(EqnarrayStar):
-    """ AMSMath """
+class _AMSEquation(eqnarray):
+    pass
+
+class _AMSEquationStar(EqnarrayStar):
+    macroName = None
+
+class align(_AMSEquation):
+    pass
+
+class AlignStar(_AMSEquation):
     macroName = 'align*'
 
-class align(eqnarray):
-    """ AMSMath """
+class gather(_AMSEquation):
+    macroName = None
+
+class GatherStar(_AMSEquation):
+    macroName = 'gather*'
+
+class falign(_AMSEquation):
+    pass
+
+class FAlignStar(_AMSEquation):
+    macroName = 'falign*'
+
+class multiline(_AMSEquation):
+    pass
+
+class MultilineStar(_AMSEquation):
+    macroName = 'multiline*'
+
+class alignat(_AMSEquation):
+    pass
+
+class AlignatStar(_AMSEquation):
+    macroName = 'alignat*'
+
+class split(_AMSEquation):
     pass
 
