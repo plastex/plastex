761807bbf64f0d9834ce7ee3ce2e439f577851f1
kesmit <kesmit>
2006-12-08 14:53:16 +0000

    Fixed up color package

--- plasTeX/Packages/color.py
+++ plasTeX/Packages/color.py
@@ -2,7 +2,49 @@
 
 from plasTeX import Command, Environment
 
-def latex2htmlcolor(arg):
+def ProcessOptions(options, document):
+    colors = {}
+    document.userdata.setPath('packages/color/colors', colors)
+    colors['red'] = latex2htmlcolor('1,0,0')
+    colors['green'] = latex2htmlcolor('0,1,0')
+    colors['blue'] = latex2htmlcolor('0,0,1')
+    
+    colors['cyan'] = latex2htmlcolor('0,1,1')
+    colors['magenta'] = latex2htmlcolor('1,0,1')
+    colors['yellow'] = latex2htmlcolor('1,1,0')
+    
+    colors['white'] = latex2htmlcolor('1')
+    colors['black'] = latex2htmlcolor('0')
+    colors['gray'] = latex2htmlcolor('0.9')
+    
+    colors['darkred'] = latex2htmlcolor('0.8,0,0')
+    colors['middlered'] = latex2htmlcolor('0.9,0,0')
+    colors['lightred'] = latex2htmlcolor('1,0,0')
+    
+    colors['darkgreen'] = latex2htmlcolor('0,0.6,0')
+    colors['middlegreen'] = latex2htmlcolor('0,0.8,0')
+    colors['lightgreen'] = latex2htmlcolor('0,1,0')
+    
+    colors['darkblue'] = latex2htmlcolor('0,0,0.8')
+    colors['middleblue'] = latex2htmlcolor('0,0,0.9')
+    colors['lightblue'] = latex2htmlcolor('0,0,1')
+    
+    colors['darkcyan'] = latex2htmlcolor('0.6,0.8,0.8')
+    colors['middlecyan'] = latex2htmlcolor('0,0.8,0.8')
+    
+    colors['darkmagenta'] = latex2htmlcolor('0.8,0.6,0.8')
+    colors['middlemagenta'] = latex2htmlcolor('1,0,0.6')
+    
+    colors['darkyellow'] = latex2htmlcolor('0.8,0.8,0.6')
+    colors['middleyellow'] = latex2htmlcolor('1,1,0.2')
+    
+    colors['darkgray'] = latex2htmlcolor('0.5')
+    colors['middlegray'] = latex2htmlcolor('0.7')
+    colors['lightgray'] = latex2htmlcolor('0.9')
+
+def latex2htmlcolor(arg, model='rgb', named={}):
+    if model == 'named':
+        return named.get(arg, '')
     if ',' in arg:
         red, green, blue = [float(x) for x in arg.split(',')]
         red = min(int(red * 255), 255)
@@ -12,17 +54,53 @@ def latex2htmlcolor(arg):
         try: 
             red = green = blue = float(arg)
         except ValueError:
-            return arg.strip()
+            try:
+                return named[arg]
+            except KeyError:
+                return arg.strip()
     return '#%.2X%.2X%.2X' % (red, green, blue)
 
-class textcolor(Command):
-    args = 'color:str self'
+class definecolor(Command):
+    args = 'name:str model:str color:str'
     def invoke(self, tex):
         a = self.parse(tex)
-        self.style['color'] = latex2htmlcolor(a['color'])
+        u = self.ownerDocument.userdata
+        colors = u.getPath('packages/color/colors')
+        colors[a['name']] = latex2htmlcolor(a['color'], a['model'], colors)
+
+class textcolor(Command):
+    args = '[ model:str ] color:str self'
+    def invoke(self, tex):
+        a = self.parse(tex)
+        self.style['color'] = latex2htmlcolor(a['color'], a['model'], 
+                  self.ownerDocument.userdata.getPath('packages/color/colors'))
 
 class color(Environment):
-    args = 'color:str'
+    args = '[ model:str ] color:str'
     def invoke(self, tex):
         a = self.parse(tex)
-        self.style['color'] = latex2htmlcolor(a['color'])
+        self.style['color'] = latex2htmlcolor(a['color'], a['model'], 
+                  self.ownerDocument.userdata.getPath('packages/color/colors'))
+
+class pagecolor(Command):
+    args = '[ model:str ] color:str'
+
+class colorbox(Command):
+    args = '[ model:str ] color:str self'
+    def invoke(self, tex):
+        a = self.parse(tex)
+        self.style['background-color'] = latex2htmlcolor(a['color'], 
+     a['model'], self.ownerDocument.userdata.getPath('packages/color/colors'))
+
+class fcolorbox(Command):
+    args = '[ model:str ] bordercolor:str color:str self'
+    def invoke(self, tex):
+        a = self.parse(tex)
+        self.style['background-color'] = latex2htmlcolor(a['color'], 
+     a['model'], self.ownerDocument.userdata.getPath('packages/color/colors'))
+        self.style['border'] = '1px solid %s' % latex2htmlcolor(
+                                          a['bordercolor'], a['model'], 
+           self.ownerDocument.userdata.getPath('packages/color/colors'))
+
+class normalcolor(Command):
+    pass
