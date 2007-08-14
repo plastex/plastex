9b31382da610049d930e7e3b3c7a76c8ac5a1942
kesmit <kesmit>
2007-08-14 17:54:18 +0000

    Incorporated Tim Arnold's changes to ams macros

--- plasTeX/Packages/amsmath.py
+++ plasTeX/Packages/amsmath.py
@@ -3,6 +3,8 @@
 from plasTeX import Command, Environment
 from plasTeX.Base.LaTeX.Arrays import Array
 from plasTeX.Base.LaTeX.Math import EqnarrayStar, equation, eqnarray
+#### Imports Added by Tim ####
+from plasTeX.Base.LaTeX.Math import math
 
 class pmatrix(Array):
     pass
@@ -16,33 +18,88 @@ class _AMSEquationStar(EqnarrayStar):
 class align(_AMSEquation):
     pass
 
-class AlignStar(_AMSEquation):
+class AlignStar(_AMSEquationStar):
     macroName = 'align*'
 
 class gather(_AMSEquation):
-    macroName = None
+    pass
 
-class GatherStar(_AMSEquation):
+class GatherStar(_AMSEquationStar):
     macroName = 'gather*'
 
 class falign(_AMSEquation):
     pass
 
-class FAlignStar(_AMSEquation):
+class FAlignStar(_AMSEquationStar):
     macroName = 'falign*'
 
 class multiline(_AMSEquation):
     pass
 
-class MultilineStar(_AMSEquation):
+class MultilineStar(_AMSEquationStar):
     macroName = 'multiline*'
 
 class alignat(_AMSEquation):
     pass
 
-class AlignatStar(_AMSEquation):
+class AlignatStar(_AMSEquationStar):
     macroName = 'alignat*'
 
 class split(_AMSEquation):
+    pass
+
+#### Added by Tim ####
+class EquationStar(_AMSEquationStar):
+    macroName = 'equation*'
+
+class aligned(_AMSEquation):
+    pass
+
+class cases(_AMSEquation):
+    pass
+
+class alignat(_AMSEquation):
+    args = 'column:int'
+class AlignatStar(_AMSEquationStar):
+    args = 'column:int'
+    macroName = 'alignat*'
+
+class flalign(_AMSEquation):
+    pass
+class FlalignStar(_AMSEquationStar):
+    macroName = 'flalign*'
+
+class subequations(_AMSEquation):
+    pass
+
+class xalignat(alignat):
+    pass
+
+class multline(multiline):
+    pass
+class MultlineStar(MultilineStar):
+    macroName = 'multline*'
+
+class matrix(Array):
+    pass
+
+class vmatrix(Array):
+    pass
+class Vmatrix(Array):
+    pass
+
+class bmatrix(Array):
+    pass
+class Bmatrix(Array):
+    pass
+
+#### Inline Math
+class smallmatrix(math):
+    pass
+
+class dddot(math):
+    pass
+
+class ddddot(math):
     pass
 
