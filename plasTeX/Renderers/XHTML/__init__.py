6815580ede23d39e8060e60b729d49ffd01a10f3
kesmit <kesmit>
2007-10-10 19:20:05 +0000

    Removed unneeded code

--- plasTeX/Renderers/XHTML/__init__.py
+++ plasTeX/Renderers/XHTML/__init__.py
@@ -23,28 +23,6 @@ class XHTML(_Renderer):
         # Remove empty paragraphs
         s = re.compile(r'<p>\s*</p>', re.I).sub(r'', s)
 
-        # Realign eqnarray.  The cells in an eqnarry can really have some
-        # deep depths.  This causes the content of those cells to overlap
-        # the content below them.  Therefore, all cells in an eqnarray
-        # are pulled up by the amount of the deepest depth.
-        def normalizeBottom(m):
-            begin, rows, end = m.group(1), m.group(2), m.group(3)
-            rows = re.compile(r'(<tr\b[^>]*>.+?</tr>)', re.I|re.S).findall(rows)
-            newrows = []
-            for i, row in enumerate(rows):
-                bottoms = {}
-                for amount, unit in re.compile(r'(?:\'|"|;)\s*bottom\s*:\s*(-?[\d\.]+)\s*(\w*)', re.I).findall(row):
-                    if unit not in bottoms:
-                        bottoms[unit] = []
-                    bottoms[unit].append(float(amount))
-                for unit in bottoms:
-                    adjust = -min(bottoms[unit])
-                    row = re.compile(r'((?:\'|"|;)\s*bottom\s*:\s*)(-?[\d\.]+)\s*%s' % unit, re.I).sub(lambda x: x.group(1) + str(float(x.group(2)) + adjust) + unit, row)
-                newrows.append(row)
-            return begin + u''.join(newrows) + end
-
-        s = re.compile(r'(<table\s+[^>]*class="eqnarray"[^>]*>\s*)(.+?)(\s*</table>)', re.I|re.S).sub(normalizeBottom, s)
-
         return s
 
 Renderer = XHTML 
