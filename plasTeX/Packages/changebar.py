73c7c57b10a6f2087bc26a6a2b69910e5e42a5bf
kesmit <kesmit>
2007-10-22 19:33:14 +0000

    Added changebar package

---
+++ plasTeX/Packages/changebar.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+from plasTeX import Command, Environment, DimenCommand, Counter
+
+def ProcessOptions(options, document):
+    context = document.context
+
+class cbstart(Command):
+    def invoke(self, tex):
+        cb = self.ownerDocument.createElement('changebar')
+        cb.macroMode = self.MODE_BEGIN
+        cb.invoke(tex)
+        return [cb]
+
+class cbend(Command):
+    def invoke(self, tex):
+        cb = self.ownerDocument.createElement('changebar')
+        cb.macroMode = self.MODE_END
+        cb.invoke(tex)
+        return [cb]
+
+class changebar(Environment):
+    args = '[ width:str ]'
+    block = True
+
+class cbdelete(Command):
+    args = '[ width:str ]'
+
+class nochangebars(Command):
+    pass
+
+class cbcolor(Command):
+    args = '[ model:str ] color:str'
+
+class changebarwidth(DimenCommand):
+    pass
+
+class deletebarwidth(DimenCommand):
+    pass
+
+class changebarsep(DimenCommand):
+    pass
+
+class changebargrey(Counter):
+    pass
+
+class outerbarstrue(Command):
+    pass
+
+class driver(Command):
+    args = 'name:str'
