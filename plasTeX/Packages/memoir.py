adc173146c47ef0d30bc63aa35728ef869fc5f6a
kesmit <kesmit>
2007-11-12 20:29:24 +0000

    Add memoir specific commands that do nothing in plasTeX

--- plasTeX/Packages/memoir.py
+++ plasTeX/Packages/memoir.py
@@ -4,3 +4,11 @@ from book import *
 
 class titleref(Command):
     args = 'label:idref'
+
+class tightlist(Command):
+    def invoke(self, tex):
+        return []
+        
+class firmlist(Command):
+    def invoke(self, tex):
+        return []
