595480d941f4575ec09457c38c01fb306e1ba831
kesmit <kesmit>
2012-03-28 15:06:56 +0000

    Fix dvisvgm

--- plasTeX/Imagers/dvisvgm.py
+++ plasTeX/Imagers/dvisvgm.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import os, re
+import os, re, glob
 import plasTeX.Imagers
 
 class DVISVGM(plasTeX.Imagers.VectorImager):
@@ -18,10 +18,17 @@ class DVISVGM(plasTeX.Imagers.VectorImager):
             rc = os.system('dvisvgm --scale=1.6 --no-fonts --output=%s --page=%d images.dvi' % (filename, page))
             if rc:
                 break
+            
+            # dvisvgm always puts "-<page-number>" on each file.  Get rid of it. 
+            try: os.rename(glob.glob(filename+'-*')[0], filename)
+            except IndexError: break
+            
             if not open(filename).read().strip():
                 os.remove(filename)
                 break
             page += 1
+            if page > len(self.images):
+                break
         return rc, None
 
 Imager = DVISVGM
