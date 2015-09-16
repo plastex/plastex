30b543d7caa7b557aeb617e72496a034db35c714
tiarno <jtim.arnold@gmail.com>
2015-09-16 10:09:44 -0400

    define readInteger value to None before read.
    othewise you get a nonobvious error message. See this question for
    details: https://github.com/tiarno/plastex/issues/19

--- plasTeX/TeX.py
+++ plasTeX/TeX.py
@@ -1581,6 +1581,7 @@ class TeX(object):
         ParameterCommand.disable()
         num = None
         sign = self.readOptionalSigns()
+        t = None
         for t in self:
             # internal/coerced integers
             if t.nodeType == Macro.ELEMENT_NODE:
