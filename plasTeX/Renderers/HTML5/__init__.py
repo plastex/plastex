a5fee215e87bfb74c95a19bc4919809adf7a8366
Patrick Massot <patrickmassot@free.fr>
2016-06-18 10:55:18 +0200

    Initial HTML5

---
+++ plasTeX/Renderers/HTML5/__init__.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import sys, os, re, codecs, plasTeX
+from plasTeX.Renderers.PageTemplate import Renderer as _Renderer
+
+class HTML5(_Renderer):
+    """ Renderer for XHTML HTML5 documents, heavily copied from XHTML renderer """
+
+    fileExtension = '.html'
+    imageTypes = ['.png','.jpg','.jpeg','.gif']
+    vectorImageTypes = ['.svg']
+
+    def processFileContent(self, document, s):
+        s = _Renderer.processFileContent(self, document, s)
+
+        # Force XHTML syntax on empty tags
+        s = re.compile(r'(<(?:hr|br|img|link|meta)\b.*?)\s*/?\s*(>)',
+                       re.I|re.S).sub(r'\1 /\2', s)
+
+        # Remove empty paragraphs
+        s = re.compile(r'<p>\s*</p>', re.I).sub(r'', s)
+
+        # Add a non-breaking space to empty table cells
+        s = re.compile(r'(<(td|th)\b[^>]*>)\s*(</\2>)', re.I).sub(r'\1&nbsp;\3', s)
+
+        return s
+
+
+Renderer = HTML5
