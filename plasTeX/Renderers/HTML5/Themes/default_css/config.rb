8a4159bc5824a2b2423509ea372948ea5589840e
Patrick Massot <patrickmassot@free.fr>
2016-06-25 09:28:10 +0200

    Set compass output dir

--- plasTeX/Renderers/HTML5/Themes/default_css/config.rb
+++ plasTeX/Renderers/HTML5/Themes/default_css/config.rb
@@ -5,7 +5,7 @@ require 'singularitygs'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
+css_dir = "../default/styles/"
 sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
