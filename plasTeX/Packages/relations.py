8f437ccb329f745ab949ada5f9d25aedd5a8246e
Patrick Massot <patrickmassot@free.fr>
2016-07-01 09:51:22 +0200

    Commence à utiliser uses dans le template

--- plasTeX/Packages/relations.py
+++ plasTeX/Packages/relations.py
@@ -21,3 +21,17 @@ class uses(Command):
         depgraph = doc.userdata.get('depgraph', DepGraph())
         depgraph += [(node, used_node) for used_node in used]
         doc.setUserData('depgraph', depgraph)
+
+class covers(Command):
+    """ \covers{labels list} """
+    args = 'labels:list:nox'
+
+    def digest(self, tokens):
+        Command.digest(self, tokens)
+        node = self.parentNode
+        labels_dict = self.ownerDocument.context.labels
+        covered = [labels_dict[label] for label in self.attributes['labels']]
+        node.setUserData('covers', covered)
+        for covered_node in covered:
+            covered_by = covered_node.userdata.get('covered_by', []) + [self]
+            covered_node.setUserData('covered_by', covered_by)
