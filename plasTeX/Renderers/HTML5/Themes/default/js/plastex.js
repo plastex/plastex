248e35fe93e05436f28bf3d45892f390bfc68eba
Patrick Massot <patrickmassot@free.fr>
2016-07-05 23:13:44 +0200

    Travail sur le menu toc

--- plasTeX/Renderers/HTML5/Themes/default/js/plastex.js
+++ plasTeX/Renderers/HTML5/Themes/default/js/plastex.js
@@ -1,10 +1,23 @@
+$.fn.toggleText = function(t1, t2){
+  if (this.text() == t1) this.text(t2);
+  else                   this.text(t1);
+  return this;
+};
+
 $(document).ready(function() {
     $("#toc-toggle").click(function() {
         $("nav.toc").toggleClass("active")
     });
 
-   $("button.uses").click(function() {
-     $(this).siblings("div.thm_uses").slideToggle()
-   })
+   $("button.uses").click(
+		   function() {
+			   $(this).siblings("div.thm_uses").slideToggle()
+		   })
+
+   $("button.expand-toc").click(
+		   function() {
+			   $(this).toggleText('x', '+');
+			   $(this).siblings("ul").slideToggle('fast')
+		   })
   });
 
