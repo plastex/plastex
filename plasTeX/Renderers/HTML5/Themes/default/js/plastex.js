c601300dc40359f45e0344fc0902dc56b9b584f1
Patrick Massot <patrickmassot@free.fr>
2016-07-17 18:45:25 +0200

    Recover from git mistake

--- plasTeX/Renderers/HTML5/Themes/default/js/plastex.js
+++ plasTeX/Renderers/HTML5/Themes/default/js/plastex.js
@@ -5,19 +5,25 @@ $.fn.toggleText = function(t1, t2){
 };
 
 $(document).ready(function() {
-    $("#toc-toggle").click(function() {
-        $("nav.toc").toggleClass("active")
-    });
+     $("#toc-toggle").click(function() {
+         $("nav.toc").toggleClass("active")
+     });
 
-   $("button.expand-toc").click(
-		   function() {
-			   $(this).toggleText('x', '+');
-			   $(this).siblings("ul").slideToggle('fast')
-		   })
+    $("span.expand-toc").click(
+            function() {
+                $(this).toggleClass('fa-close').toggleClass('fa-plus');
+                $(this).siblings("ul").slideToggle('fast')
+            })
 
-   $("button.uses").click(
-		   function() {
-			   $(this).siblings("div.thm_uses").slideToggle()
-		   })
+    $("button.uses").click(
+           function() {
+               $(this).siblings("div.thm_uses").slideToggle()
+           })
+    
+    $("div.proof_heading").click(
+           function() {
+               $(this).children('span.expand-proof').toggleClass('fa-close').toggleClass('fa-plus');
+               $(this).siblings("div.proof_content").slideToggle()
+           })
   });
 
