7a80c5fa2b21b8c259c80075c57efd91dc88db3d
Patrick Massot <patrickmassot@free.fr>
2016-07-24 14:59:42 +0200

    Switch to SVG icons

--- plasTeX/Renderers/HTML5/Themes/default/js/plastex.js
+++ plasTeX/Renderers/HTML5/Themes/default/js/plastex.js
@@ -5,14 +5,30 @@ $.fn.toggleText = function(t1, t2){
 };
 
 $(document).ready(function() {
+     var icon = function($icon, $class, $id) {
+		 if ($id) {
+			 $id = ' id="'+$id+'"';
+		 } else {
+			 $id = '';
+		 }
+
+		 return '<svg'+ $id + ' class="icon icon-' + $icon + ' ' + $class +'"><use xlink:href="symbol-defs.svg#icon-'+$icon+'"></use></svg>'
+	 };
+
      $("#toc-toggle").click(function() {
          $("nav.toc").toggleClass("active")
      });
 
-    $("span.expand-toc").click(
+    $("nav.toc").on("click", "svg.expand-toc",
             function() {
-                $(this).toggleClass('fa-close').toggleClass('fa-plus');
-                $(this).siblings("ul").slideToggle('fast')
+				$(this).siblings("ul").slideToggle('fast');
+
+				if ($(this).attr('class') == 'icon icon-cross expand-toc') {
+                  $(this).replaceWith(icon('plus', 'expand-toc'));
+				} else {
+                  $(this).replaceWith(icon('cross', 'expand-toc'));
+				};
+
             })
 
     $("button.uses").click(
