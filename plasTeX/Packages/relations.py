f174225bc78918052eef3b67bf3617cf09b8f310
Patrick Massot <patrick.massot@polytechnique.edu>
2016-08-29 18:10:42 +0200

    Showmore + demonstrations

--- plasTeX/Packages/relations.py
+++ plasTeX/Packages/relations.py
@@ -45,6 +45,21 @@ class covers(Command):
                 covered.setUserData('covered_by', covered_by + [node])
         doc.post_parse_cb.append(update_covered)
 
+class proves(Command):
+    """ \proves{label} """
+    args = 'label:str'
+
+    def digest(self, tokens):
+        Command.digest(self, tokens)
+        node = self.parentNode
+        doc = self.ownerDocument
+        def update_proved():
+            labels_dict = doc.context.labels
+            proved = labels_dict[self.attributes['label']]
+            node.setUserData('proves', proved)
+            proved.userdata['proved_by'] = node
+        doc.post_parse_cb.append(update_proved)
+
 
 class ThmReport(object):
     """"""
--- plasTeX/Renderers/HTML5/Themes/default/js/plastex.js
+++ plasTeX/Renderers/HTML5/Themes/default/js/plastex.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
      $("#toc-toggle").click(function() {
          $("nav.toc").toggleClass("active")
      });
-     
+
      $(".close-toc").click(function() {
          $("nav.toc").removeClass("active")
      });
@@ -39,7 +39,7 @@ $(document).ready(function() {
            function() {
                $(this).siblings("div.thm_uses").slideToggle()
            })
-    
+
     $("div.proof_heading").click(
            function() {
                var expand_icon = $(this).children('svg.expand-proof');
@@ -48,9 +48,26 @@ $(document).ready(function() {
 							 } else {
 												 expand_icon.replaceWith(icon('cross', 'expand-proof'));
 							 };
-							 
+
                $(this).siblings("div.proof_content").slideToggle()
            })
+
+		$("a.proof").click(
+				function() {
+					var ref= $(this).attr('href').split('#')[1];
+					var proof = $('#'+ref)
+					proof.children('.proof_content').each(
+							function() { 
+								var proof_content = $(this)
+								proof_content.show().addClass('hilite')
+								setTimeout(function(){
+									proof_content.removeClass('hilite')
+									}, 1000);
+							})
+          var expand_icon = proof.find('svg.expand-proof');
+					expand_icon.replaceWith(icon('cross', 'expand-proof'));
+					
+				})
 
     $("li.quizz-qright").click(
            function() {
@@ -82,5 +99,65 @@ $(document).ready(function() {
         function() {
           $(this).parent().parent().parent().hide();
         })
+
+		var showmore_level = 1;
+
+		showmore_update = function() {
+			switch(showmore_level) {
+				case 0:
+				 $("svg#showmore-minus").hide();
+				 $("svg#showmore-plus").show();
+				 $("div.content > p").each(
+						 function(){
+							 $(this).hide();
+						 });
+				 $("div.proof_wrapper").each(
+						 function(){
+							 $(this).hide();
+						 });
+				 break;
+				case 1:
+				 $("svg#showmore-minus").show();
+				 $("svg#showmore-plus").show();
+				 $("div.content > p").each(
+						 function(){
+							 $(this).show();
+						 });
+				 $("div.proof_content").each(
+						 function(){
+							 $(this).hide();
+						 });
+				 break;
+				case 2:
+				 $("svg#showmore-minus").show();
+				 $("svg#showmore-plus").hide();
+				 $("div.content > p").each(
+						 function(){
+							 $(this).show();
+						 });
+				 $("div.proof_content").each(
+						 function(){
+							 $(this).show();
+						 });
+			}
+		};
+
+		$("svg#showmore-minus").click(
+				function() {
+					if (showmore_level > 0) {
+						showmore_level -= 1;
+						showmore_update();
+					}
+				})
+
+		$("svg#showmore-plus").click(
+				function() {
+					if (showmore_level < 2) {
+						showmore_level += 1;
+						showmore_update();
+					}
+				})
+
+		$("div.proof_content p:last-child").append('<span class="qed">□</span>')
   });
 
