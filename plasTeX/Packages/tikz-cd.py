53053d9e3fe13f46cc7da9148bb112b7057fa97c
Patrick Massot <patrick.massot@polytechnique.edu>
2016-11-08 21:45:16 +0100

    TikZ support

--- plasTeX/Packages/tikz-cd.py
+++ plasTeX/Packages/tikz-cd.py
@@ -5,9 +5,11 @@ from bs4 import BeautifulSoup
 
 from plasTeX import Environment, Command
 
+from plasTeX.Packages import tikz
+
 class tikzcd(Environment):
     """
-    A tikcz-cd diagram whose content will be converted in the processFileContent callback.
+    A tikz-cd diagram whose content will be converted in the processFileContent callback.
     """
 
 class ar(Command):
@@ -37,43 +39,6 @@ class ular(Command):
 class urar(Command):
     pass
 
-def tikzConvert(document, content):
-    tmp_dir = document.userdata['tikzcd']['tmp_dir']
-    working_dir = document.userdata['working-dir']
-    target_dir = os.path.join(working_dir, document.config['files']['directory'], 'images')
-    template = document.userdata['tikzcd']['template']
-
-    cwd = os.getcwd()
-    os.chdir(tmp_dir)
-    soup = BeautifulSoup(content, "html.parser")
-    encoding = soup.original_encoding
-
-    tikzcds = soup.findAll('tikzcd')
-    for tikzcd in tikzcds:
-        cd_id = tikzcd.attrs['id']
-        basepath = os.path.join(tmp_dir, cd_id)
-        texpath = basepath + '.tex'
-        pdfpath = basepath + '.pdf'
-        svgpath =  basepath + '.svg'
-
-        context = {'diagram': tikzcd.text.strip() }
-        template.stream(**context).dump(texpath, encoding)
-
-        os.system('xelatex ' + texpath)
-        os.system('pdf2svg '+ pdfpath + ' ' + svgpath)
-        os.system('mv ' + svgpath + ' ' + target_dir)
-
-        obj = soup.new_tag(
-                'object', 
-                type='image/svg+xml',
-                data='images/' + cd_id + '.svg')
-        obj.string = document.context.terms.get(
-                'Commutative diagram',
-                'Commutative diagram')
-        obj.attrs['class'] = 'tikzcd' 
-        tikzcd.replace_with(obj)
-    os.chdir(cwd)
-    return unicode(soup)
 
 def ProcessOptions(options, document):
     """This is called when the package is loaded."""
@@ -83,11 +48,16 @@ def ProcessOptions(options, document):
             template = file.read()
     else:
         template = u"\\documentclass{standalone}\n\\usepackage{tikz-cd}" + \
-                   u"\\begin{document}{{ diagram }}\\end{document}"
+                   u"\\begin{document}{{ tikzpicture }}\\end{document}"
     document.userdata['tikzcd'] = {
             'template': Template(template),
             'tmp_dir': tempfile.mkdtemp()}
-    processFileContents = document.userdata.get('processFileContents', [])
-    processFileContents.append(tikzConvert)
-    document.userdata['processFileContents'] = processFileContents
 
+    def convert(document, content):
+        return tikz.tikzConvert(
+                document, 
+                content, 
+                'tikzcd', 
+                'Commutative diagram')
+
+    document.userdata.setdefault('processFileContents', []).append(convert)
---
+++ plasTeX/Packages/tikz.py
@@ -0,0 +1,78 @@
+"""
+Implement the tikz package for html output.
+The original tikz and xelatex do most of the work and then pdf2svg turns it
+into a svg image.
+Needs Beautiful Soup, Jinja2 and pdf2svg
+"""
+
+import os
+import tempfile
+from jinja2 import Template
+from bs4 import BeautifulSoup
+
+from plasTeX import Environment, Command
+
+class tikzpicture(Environment):
+    """
+    A tikz picture whose content will be converted in the processFileContent callback.
+    """
+    class draw(Command):
+        def normalize(self, charsubs=[]):
+            """ Normalize, but don't allow character substitutions """
+            return Environment.normalize(self, [])
+
+def tikzConvert(document, content, envname, placeholder):
+    tmp_dir = document.userdata[envname]['tmp_dir']
+    working_dir = document.userdata['working-dir']
+    target_dir = os.path.join(working_dir, document.config['files']['directory'], 'images')
+    template = document.userdata[envname]['template']
+
+    cwd = os.getcwd()
+    os.chdir(tmp_dir)
+    soup = BeautifulSoup(content, "html.parser")
+    encoding = soup.original_encoding
+
+    envs = soup.findAll(envname)
+    for env in envs:
+        cd_id = env.attrs['id']
+        basepath = os.path.join(tmp_dir, cd_id)
+        texpath = basepath + '.tex'
+        pdfpath = basepath + '.pdf'
+        svgpath =  basepath + '.svg'
+
+        context = {'tikzpicture': env.text.strip() }
+        template.stream(**context).dump(texpath, encoding)
+
+        os.system('xelatex ' + texpath)
+        os.system('pdf2svg '+ pdfpath + ' ' + svgpath)
+        os.system('mv ' + svgpath + ' ' + target_dir)
+
+        obj = soup.new_tag(
+                'object', 
+                type='image/svg+xml',
+                data='images/' + cd_id + '.svg')
+        obj.string = document.context.terms.get(
+                placeholder,
+                placeholder)
+        obj.attrs['class'] = envname
+        env.replace_with(obj)
+    os.chdir(cwd)
+    return unicode(soup)
+
+def ProcessOptions(options, document):
+    """This is called when the package is loaded."""
+    
+    if 'template' in options:
+        with open(options['template'], "r") as file:
+            template = file.read()
+    else:
+        template = u"\\documentclass{standalone}\n\\usepackage{tikz}" + \
+                   u"\\begin{document}{{ tikzpicture }}\\end{document}"
+    document.userdata['tikzpicture'] = {
+            'template': Template(template),
+            'tmp_dir': tempfile.mkdtemp()}
+
+    def convert(document, content):
+        return tikzConvert(document, content, 'tikzpicture', 'TikZ picture')
+
+    document.userdata.setdefault('processFileContents', []).append(convert)
