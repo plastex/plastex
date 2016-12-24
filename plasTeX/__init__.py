eb34510d7805b8b732db1234fe2d6145eae96660
Patrick Massot <patrickmassot@free.fr>
2016-12-24 21:55:56 +0100

    Fix #45 by adding  NoCharSubEnvironment
    The goal is to stop character substitutions in certain environments
    
    (cherry picked from commit f43dfc0e93800dedca0f1b766c2180b2b26ed0d8)

--- plasTeX/__init__.py
+++ plasTeX/__init__.py
@@ -900,6 +900,28 @@ class Environment(Macro):
         if dopars:
             self.paragraphs()
 
+
+class NoCharSubEnvironment(Environment):
+    """
+    A subclass of Environment which prevents character substitution inside
+    itself.
+    """
+    def __init__(self, *args, **kwargs):
+        # Will hold the owner document charsubs to restore it at the end
+        self.charsubs = []
+        super(NoCharSubEnvironment, self).__init__(*args, **kwargs)
+
+    def invoke(self, tex):
+        # The goal is to prevent any character substitution while handling a
+        # this environment.
+        doc = self.ownerDocument
+        if self.macroMode == Macro.MODE_BEGIN:
+            self.charsubs = doc.charsubs
+            doc.charsubs = []
+        elif self.macroMode == Macro.MODE_END:
+            doc.charsubs = self.charsubs
+        super(NoCharSubEnvironment, self).invoke(tex)
+
 class IgnoreCommand(Command):
     """
     This command will be parsed, but will not go to the output stream
