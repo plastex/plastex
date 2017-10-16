bbe6581b72608a242ea0b3a68dc70dc40309084c
Patrick Massot <patrickmassot@free.fr>
2017-10-16 21:42:36 +0200

    Change temp directory strategy
    Alltt currently fails with mysterious I/O error on temp file. Google
    suggests using tempfile.mkdtemp() on Travis is a recipe for pain.
    So I try to turn this test module into pytest style (and it's sooo
    much shorter and more elegant).

--- unittests/Packages/Alltt.py
+++ unittests/Packages/Alltt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import unittest, re, os, tempfile, shutil
+import unittest, re, os 
 from plasTeX.TeX import TeX
 from unittest import TestCase
 
@@ -14,93 +14,82 @@ except ImportError:
     from BeautifulSoup import BeautifulSoup as Soup
 
 
-class Alltt(TestCase):
+def runDocument(content):
+    """
+    Compile a document with the given content
 
-    def runDocument(self, content):
-        """
-        Compile a document with the given content
+    Arguments:
+    content - string containing the content of the document
 
-        Arguments:
-        content - string containing the content of the document
+    Returns: TeX document
 
-        Returns: TeX document
+    """
+    tex = TeX()
+    tex.disableLogging()
+    tex.input(r'''\document{article}\usepackage{alltt}\begin{document}%s\end{document}''' % content)
+    return tex.parse()
 
-        """
-        tex = TeX()
-        tex.disableLogging()
-        tex.input(r'''\document{article}\usepackage{alltt}\begin{document}%s\end{document}''' % content)
-        return tex.parse()
+def runPreformat(content, tmpdir):
+    """
+    This method compiles and renders a document fragment and
+    returns the result
 
-    def runPreformat(self, content):
-        """
-        This method compiles and renders a document fragment and
-        returns the result
+    Arguments:
+    content - string containing the document fragment
 
-        Arguments:
-        content - string containing the document fragment
+    Returns: content of output file
 
-        Returns: content of output file
+    """
+    # Create document file
+    document = r'\documentclass{article}\usepackage{alltt}\begin{document}%s\end{document}' % content
+    srcfile = tmpdir.join('longtable.tex')
+    srcfile.write(document)
 
-        """
-        # Create document file
-        document = r'\documentclass{article}\usepackage{alltt}\begin{document}%s\end{document}' % content
-        tmpdir = tempfile.mkdtemp()
-        filename = os.path.join(tmpdir, 'longtable.tex')
-        open(filename, 'w').write(document)
+    # Run plastex on the document
+    log = os.popen(
+            'plastex -d %s %s 2>&1' % (str(tmpdir), str(srcfile))).read()
+    assert '[ index.html ]' in log, 'No file was generated - %s' % log
 
-        # Run plastex on the document
-        log = os.popen('plastex -d %s %s 2>&1' % (tmpdir, filename)).read()
-        assert '[ index.html ]' in log, 'No file was generated - %s' % log
+    # Get output file
+    output = tmpdir.join('index.html').read()
 
-        # Get output file
-        output = open(os.path.join(tmpdir, 'index.html')).read()
+    return Soup(output).findAll('pre')[-1]
 
-        # Clean up
-        shutil.rmtree(tmpdir)
-        os.remove('longtable.paux')
+def test_simple(tmpdir):
+    text = '''\\begin{alltt}\n   line 1\n   line 2\n   line 3\n\\end{alltt}'''
+    lines = ['', '   line 1', '   line 2', '   line 3', '']
 
-        return Soup(output).findAll('pre')[-1]
+    # Test text content of node
+    out = runDocument(text).getElementsByTagName('alltt')[0]
 
-    def testSimple(self):
-        text = '''\\begin{alltt}\n   line 1\n   line 2\n   line 3\n\\end{alltt}'''
-        lines = ['', '   line 1', '   line 2', '   line 3', '']
+    plines = out.textContent.split('\n')
+    assert lines == plines, 'Content doesn\'t match - %s - %s' % (lines, plines)
 
-        # Test text content of node
-        out = self.runDocument(text).getElementsByTagName('alltt')[0]
+    # Test text content of rendering
+    out = runPreformat(text, tmpdir)
 
-        plines = out.textContent.split('\n')
-        assert lines == plines, 'Content doesn\'t match - %s - %s' % (lines, plines)
+    plines = out.string.split('\n')
+    assert lines == plines, 'Content doesn\'t match - %s - %s' % (lines, plines)
 
-        # Test text content of rendering
-        out = self.runPreformat(text)
+def test_commands(tmpdir):
+    text = '''\\begin{alltt}\n   line 1\n   \\textbf{line} 2\n   \\textit{line 3}\n\\end{alltt}'''
+    lines = ['', '   line 1', '   line 2', '   line 3', '']
 
-        plines = out.string.split('\n')
-        assert lines == plines, 'Content doesn\'t match - %s - %s' % (lines, plines)
+    # Test text content of node
+    out = runDocument(text).getElementsByTagName('alltt')[0]
 
-    def testCommands(self):
-        text = '''\\begin{alltt}\n   line 1\n   \\textbf{line} 2\n   \\textit{line 3}\n\\end{alltt}'''
-        lines = ['', '   line 1', '   line 2', '   line 3', '']
+    plines = out.textContent.split('\n')
+    assert lines == plines, 'Content doesn\'t match - %s - %s' % (lines, plines)
 
-        # Test text content of node
-        out = self.runDocument(text).getElementsByTagName('alltt')[0]
+    bf = out.getElementsByTagName('textbf')[0]
+    assert bf.textContent == 'line', 'Bold text should be "line", but it is "%s"' % bf.textContent
 
-        plines = out.textContent.split('\n')
-        assert lines == plines, 'Content doesn\'t match - %s - %s' % (lines, plines)
+    it = out.getElementsByTagName('textit')[0]
+    assert it.textContent == 'line 3', 'Italic text should be "line 3", but it is "%s"' % it.textContent
 
-        bf = out.getElementsByTagName('textbf')[0]
-        assert bf.textContent == 'line', 'Bold text should be "line", but it is "%s"' % bf.textContent
+    # Test rendering
+    out = runPreformat(text, tmpdir)
 
-        it = out.getElementsByTagName('textit')[0]
-        assert it.textContent == 'line 3', 'Italic text should be "line 3", but it is "%s"' % it.textContent
+    assert out.b.string == 'line', 'Bold text should be "line", but it is "%s"' % out.b.string
 
-        # Test rendering
-        out = self.runPreformat(text)
-
-        assert out.b.string == 'line', 'Bold text should be "line", but it is "%s"' % out.b.string
-
-        assert out.i.string == 'line 3', 'Italic text should be "line 3", but it is "%s"' % out.i.string
-
-
-if __name__ == '__main__':
-    unittest.main()
-
+    assert out.i.string == 'line 3', 'Italic text should be "line 3", but it is "%s"' % out.i.string
