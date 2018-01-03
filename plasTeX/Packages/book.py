9aa0aece794b690cfb52e796f3a45b6e73c8aa83
Pieter Belmans <pieterbelmans@gmail.com>
2018-01-03 13:40:40 +0100

    Chapter counter shouldn't reset with new part
    A MWE shows that this is not what happens in TeX.

--- plasTeX/Packages/book.py
+++ plasTeX/Packages/book.py
@@ -13,7 +13,7 @@ def ProcessOptions(options, document):
     # Sections
     context.newcounter('part', resetby='volume',
                        format='$part')
-    context.newcounter('chapter', resetby='part',
+    context.newcounter('chapter', resetby='volume',
                        format='$chapter')
     context.newcounter('section', resetby='chapter',
                        format='${thechapter}.${section}')
