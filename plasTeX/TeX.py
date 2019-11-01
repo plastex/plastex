plastex/plastex

commit 2decbb1091471b39a67dc32ca85951a1496162e9

cleans up top-level files

plasTeX/TeX.py
@@ -60,7 +60,7 @@ class TeX(object):
     """
     documentClass = TeXDocument
 
-    def __init__(self, ownerDocument=None, file=None):
+    def __init__(self, ownerDocument=None, myfile=None):
         if ownerDocument is None:
             ownerDocument = self.documentClass()
         self.ownerDocument = ownerDocument
@@ -110,10 +110,10 @@ class TeX(object):
         self.currentInput = (0,0)
 
         self.jobname = None
-        if file is not None:
+        if myfile is not None:
 
             # Filename
-            if isinstance(file, str):
+            if isinstance(myfile, str):
                 '''
                 if config has no files structure
                 or encoding is not specified
@@ -128,15 +128,15 @@ class TeX(object):
                 if encoding in ['utf8', 'utf-8', 'utf_8']:
                     encoding = 'utf_8_sig'
 
-                fname = self.kpsewhich(file)
-                fd = open(fname, encoding=encoding)
-                self.input(fd)
-                self.jobname = os.path.basename(os.path.splitext(file)[0])
+                fname = self.kpsewhich(myfile)
+                with open(fname, encoding=encoding) as fd:
+                    self.input(fd.read())
+                self.jobname = os.path.basename(os.path.splitext(myfile)[0])
 
             # File object
             else:
-                self.input(file)
-                self.jobname = os.path.basename(os.path.splitext(file.name)[0])
+                self.input(myfile)
+                self.jobname = os.path.basename(os.path.splitext(myfile.name)[0])
 
     def input(self, source):
         """
@@ -171,12 +171,12 @@ class TeX(object):
         if self.inputs:
             self.currentInput = self.inputs[-1]
 
-    def loadPackage(self, file, options={}):
+    def loadPackage(self, myfile, options={}):
         """
         Load a LaTeX package
 
         Required Arguments:
-        file -- name of the file to load
+        myfile -- name of the file to load
 
         Keyword Arguments:
         options -- options passed to the macro which is loading the package
@@ -185,7 +185,7 @@ class TeX(object):
         config = self.ownerDocument.config
 
         try:
-            path = self.kpsewhich(file)
+            path = self.kpsewhich(myfile)
         except OSError as msg:
             log.warning(msg)
             return False
@@ -201,7 +201,6 @@ class TeX(object):
                 # the output document.
                 flag = plasTeX.Command()
                 self.pushToken(flag)
-                encoding = config['files']['input-encoding']
                 self.input(f)
                 self.ownerDocument.context.packages[file] = options or {}
                 for tok in self:
@@ -210,9 +209,9 @@ class TeX(object):
 
         except (OSError, IOError, TypeError) as msg:
             if msg:
-                msg = ' ( %s )' % msg
+                msg = ' ( %s )' % str(msg)
             # Failed to load LaTeX style file
-            log.warning('Error opening package "%s"%s', file, msg)
+            log.warning('Error opening package "%s"%s', myfile, msg)
             status.info(' ) ')
             return False
 
@@ -344,9 +343,10 @@ class TeX(object):
 #                       log.info('expanding %s %s', token.macroName, ''.join([x.source for x in tokens]))
                         pushTokens(tokens)
                     continue
-                except Exception as msg:
-                    if str(msg).strip():
-                        msg = ' (%s)' % str(msg).strip()
+                except Exception as message:
+                    msg = str(message)
+                    if msg.strip():
+                        msg = ' (%s)' % msg.strip()
                     log.error('Error while expanding "%s"%s%s',
                               token.macroName, self.lineInfo, msg)
                     raise
@@ -432,9 +432,10 @@ class TeX(object):
                     item.parentNode = output
                     item.digest(tokens)
                 output.append(item)
-        except Exception as msg:
-            if str(msg).strip():
-               msg = ' (%s)' % str(msg).strip()
+        except Exception as message:
+            msg = str(message)
+            if msg.strip():
+               msg = ' (%s)' % msg.strip()
             log.error('An error occurred while building the document object%s%s', self.lineInfo, msg)
             raise
 
@@ -448,7 +449,7 @@ class TeX(object):
         text -- string containing text to be tokenized
 
         """
-        return [Other(x) for x in str(text)]
+        return [Other(x) for x in text]
 
     def pushToken(self, token):
         """
@@ -878,16 +879,16 @@ class TeX(object):
             source = [t]
             # A [ ... ], ( ... ), etc. grouping was found
             if t.catcode != Token.CC_ESCAPE and \
-               (t == begin or str(t) == str(begin)):
+               (t == begin or t == 'begin'):
                 level = 1
                 for t in tokens:
                     source.append(t)
                     if t.catcode != Token.CC_ESCAPE and \
-                       (t == begin or str(t) == str(begin)):
+                       (t == begin or t == 'begin'):
                         toks.append(t)
                         level += 1
                     elif t.catcode != Token.CC_ESCAPE and \
-                         (t == end or str(t) == str(end)):
+                         (t == end or t == 'end'):
                         level -= 1
                         if level == 0:
                             break
@@ -967,7 +968,7 @@ class TeX(object):
             pass
 
         # Could not find specified type
-        elif dtype not in argtypes:
+        elif dtype not in list(argtypes.keys()):
             log.warning('Could not find datatype "%s"' % dtype)
             pass
 
@@ -1340,6 +1341,15 @@ class TeX(object):
         else:
             TEXINPUTS = os.environ.get("TEXINPUTS",'')
             os.environ["TEXINPUTS"] = "%s%s%s%s" % (srcDir, os.path.pathsep, TEXINPUTS, os.path.pathsep)
+
+        fullname = ''
+        paths = os.environ["TEXINPUTS"].split(os.path.pathsep)
+        for path in [x for x in paths if x]:
+            if name in os.listdir(path):
+                fullname = os.path.join(path,name)
+                break
+        if fullname:
+            return fullname
 
         try:
             program = self.ownerDocument.config['general']['kpsewhich']

plasTeX/ConfigManager/__init__.py
@@ -16,8 +16,7 @@ followed by a bunch of imaginary command line options and arguments.
 """
 
 import sys, string, re, urllib.request, copy, types, os
-from plasTeX.dictutils import ordereddict
-from collections import UserList, UserDict
+from collections import UserList, UserDict, OrderedDict
 from textwrap import wrap
 
 __all__ = ['ConfigManager','BooleanOption','IntegerOption','CompoundOption',
@@ -1506,11 +1505,11 @@ class ConfigManager(UserDict, object):
         return s
 
 
-class CommandLineManager(ordereddict):
+class CommandLineManager(OrderedDict):
    """ Command-Line Argument Manager """
 
    def __init__(self, data={}):
-      ordereddict.__init__(self, data)
+      OrderedDict.__init__(self, data)
       self._associations = {}
 
    def usage(self):
@@ -1526,7 +1525,7 @@ class CommandLineManager(ordereddict):
       """ Return boolean indicating if config requires an argument """
       if not self: return 0
       for key in self:
-         item = ordereddict.__getitem__(self, key)
+         item = OrderedDict.__getitem__(self, key)
          if isinstance(item, GenericArgument):
              if item.mandatory:
                  return 1
@@ -1540,7 +1539,7 @@ class CommandLineManager(ordereddict):
       if not self: return self
 
       for key in self:
-         item = ordereddict.__getitem__(self, key)
+         item = OrderedDict.__getitem__(self, key)
          association = self._associations.get(key, None)
          if isinstance(item, ConfigManager):
             if association:
@@ -1569,12 +1568,12 @@ class CommandLineManager(ordereddict):
              'subclasses of GenericArgument'
       if hasattr(item, 'name') and item.name is None:
          item.name = key
-      ordereddict.__setitem__(self, key, item)
+      OrderedDict.__setitem__(self, key, item)
 
    def __getitem__(self, key):
       if type(key) == slice:
          return self.__getslice__(key.start, key.stop)
-      item = ordereddict.__getitem__(self, key)
+      item = OrderedDict.__getitem__(self, key)
       if isinstance(item, ConfigManager):
          return item
       else:
@@ -1596,6 +1595,3 @@ from plasTeX.ConfigManager.Directories import OutputDirectoryOption, InputDirect
 from plasTeX.ConfigManager.Files import OutputFileArgument, InputFileArgument
 from plasTeX.ConfigManager.Directories import OutputDirectoryArgument, InputDirectoryArgument
 from plasTeX.ConfigManager.Counted import CountedOption, CountedArgument
-
-
-

plasTeX/Context.py
@@ -52,13 +52,13 @@ class ContextItem(dict):
         if dict.has_key(self, key):
             return True
         if self.parent is not None:
-            return key in self.parent
+            return key in list(self.parent.keys())
 
     __contains__ = has_key
 
     def keys(self):
         keys = {}
-        for key in dict.keys(self):
+        for key in list(dict.keys(self)):
             keys[key] = 0
         if self.parent is not None:
             for key in list(self.parent.keys()):
@@ -96,8 +96,8 @@ class LanguageParser(object):
         for fname in files:
             if not os.path.isfile(fname):
                 continue
-            self.parser = expat.ParserCreate(encoding)
 
+            self.parser = expat.ParserCreate(encoding)
             self.parser.StartElementHandler = self.startElement
             self.parser.EndElementHandler = self.endElement
             self.parser.CharacterDataHandler = self.charData
@@ -112,7 +112,7 @@ class LanguageParser(object):
         for key, value in list(self.data.items()):
             if '-' in key:
                 major, minor = key.split('-',1)
-                if major in self.data:
+                if major in list(self.data.keys()):
                     majordict = self.data[major]
                     for mkey, mvalue in list(majordict.items()):
                         if mkey not in value:
@@ -233,7 +233,7 @@ class Context(object):
         if os.path.exists(filename):
             try:
                 d = pickle.load(open(filename,'rb'))
-                if rtype not in d:
+                if rtype not in list(d.keys()):
                     d[rtype] = {}
             except:
                 os.remove(filename)
@@ -307,7 +307,7 @@ class Context(object):
 
             LanguageParser(self.languages).parse(reversed(files))
 
-        if lang in self.languages:
+        if lang in list(self.languages.keys()):
             self.currentLanguage = lang
             self.newcommand('languagename', definition=lang)
             self.terms = self.languages[lang]
@@ -360,7 +360,7 @@ class Context(object):
                         if data['number'] is not None:
                             value = chr(int(data['number']))
                         else:
-                            value = str(data['string'])
+                            value = data['string']
                         macros[name] = type(name, (baseclass,),
                                                     {'str': value})
                         continue
@@ -391,7 +391,7 @@ class Context(object):
         module = os.path.splitext(file)[0]
         # pu.db
         # See if it has already been loaded
-        if module in self.packages:
+        if module in list(self.packages.keys()):
             return True
 
         packagesini = os.path.join(os.path.dirname(plasTeX.Packages.__file__),
@@ -459,7 +459,7 @@ class Context(object):
         #print label, ''.join(self.currentlabel.ref[:])
 
         # Resolve any outstanding references to this object
-        if label in self.refs and label in self.labels:
+        if label in list(self.refs.keys()) and label in (self.labels.keys()):
             for obj in self.refs[label]:
                 for key, value in list(obj.idref.items()):
                     if value.id != label:
@@ -485,12 +485,12 @@ class Context(object):
             return
 
         # Resolve ref if label already exists
-        if label in self.labels:
+        if label in list(self.labels.keys()):
             obj.idref[name] = self.labels[label]
             return
 
         # If the label doesn't exist, store away the object for later
-        if label not in self.refs:
+        if label not in list(self.refs.keys()):
             self.refs[label] = []
         self.refs[label].append(obj)
 
@@ -515,7 +515,7 @@ class Context(object):
         # Didn't find it, so generate a new class
         if self.warnOnUnrecognized and not self.isMathMode:
             log.warning('unrecognized command/environment: %s', key)
-        self[key] = newclass = type(str(key), (plasTeX.UnrecognizedMacro,), {})
+        self[key] = newclass = type(key, (plasTeX.UnrecognizedMacro,), {})
         return newclass
 
     def push(self, context=None):
@@ -789,9 +789,8 @@ class Context(object):
         initial -- initial value for the counter
 
         """
-        name = str(name)
         # Counter already exists
-        if name in self.counters:
+        if name in list(self.counters.keys()):
             macrolog.debug('counter %s already defined', name)
             return
         self.counters[name] = plasTeX.Counter(self, name, resetby, initial)
@@ -824,7 +823,6 @@ class Context(object):
         initial -- value to initialize to
 
         """
-        name = str(name)
         # Generate a new count class
         macrolog.debug('creating count %s', name)
         newclass = type(name, (plasTeX.CountCommand,),
@@ -842,7 +840,6 @@ class Context(object):
         initial -- value to initialize to
 
         """
-        name = str(name)
         # Generate a new dimen class
         macrolog.debug('creating dimen %s', name)
         newclass = type(name, (plasTeX.DimenCommand,),
@@ -860,7 +857,6 @@ class Context(object):
         initial -- value to initialize to
 
         """
-        name = str(name)
         # Generate a new glue class
         macrolog.debug('creating dimen %s', name)
         newclass = type(name, (plasTeX.GlueCommand,),
@@ -878,7 +874,6 @@ class Context(object):
         initial -- value to initialize to
 
         """
-        name = str(name)
         # Generate a new muglue class
         macrolog.debug('creating muskip %s', name)
         newclass = type(name, (plasTeX.MuGlueCommand,),
@@ -899,9 +894,9 @@ class Context(object):
         initial -- initial value of the 'if' command
 
         """
-        name = str(name)
+
         # \if already exists
-        if name in self:
+        if name in list(self.keys()):
             macrolog.debug('if %s already defined', name)
             return
 
@@ -936,8 +931,6 @@ class Context(object):
             c.newcommand('foo', 2, r'{\\bf #1#2}', opt='myprefix')
 
         """
-        # name = str(name)
-        #pu.db
         # Macro already exists
         if name in list(self.keys()):
             if not issubclass(self[name], (plasTeX.NewCommand, plasTeX.Definition)):
@@ -979,9 +972,8 @@ class Context(object):
             c.newenvironment('mylist', 0, (r'\\begin{itemize}', r'\\end{itemize}'))
 
         """
-        name = str(name)
         # Macro already exists
-        if name in self:
+        if name in list(self.keys()):
             if not issubclass(self[name], (plasTeX.NewCommand,
                                            plasTeX.Definition)):
                 return
@@ -1033,7 +1025,6 @@ class Context(object):
             c.newdef('put', '(#1,#2)#3', '\\dostuff{#1}{#2}{#3}')
 
         """
-        name = str(name)
         # Macro already exists
 #       if self.has_key(name):
 #           if not issubclass(self[name], (plasTeX.NewCommand,
@@ -1076,7 +1067,6 @@ class Context(object):
         num -- character number to use
 
         """
-        name = str(name)
         # Generate a new chardef class
         macrolog.debug('creating chardef (8-bit) %s', name)
         newclass = type(name, (plasTeX.Command,),
@@ -1092,7 +1082,6 @@ class Context(object):
         num -- character number to use
 
         """
-        name = str(name)
         # Generate a new chardef class
         macrolog.debug('creating mathchardef (16-bit) %s', name)
         newclass = type(name, (plasTeX.Command,),

plasTeX/Filenames.py
@@ -4,62 +4,62 @@ import re, string, os.path
 
 class Filenames(object):
 
-    def __init__(self, spec, charsub=[], vars={}, extension='', invalid={}):
+    def __init__(self, spec, charsub=[], variables={}, extension='', invalid={}):
         """
-        Generate filenames based on the `spec' and using the given vars
-    
+        Generate filenames based on the `spec' and using the given variables
+
         Arguments:
         spec -- string containing filename specifier.  The filename specifier
-            is a list of space separated names.  Each name in the list is 
+            is a list of space separated names.  Each name in the list is
             returned once.  An example is shown below::
-    
+
                 index.html toc.html file1.html file2.html
-    
-            These filenames can also contain variables as described in 
+
+            These filenames can also contain variables as described in
             Python's string Templates (e.g. $title, ${id}).  These variables
-            come from the `vars' variable.  One special variable is
+            come from the `variables' variable.  One special variable is
             $num.  This value in generated dynamically whenever a filename
-            with $num is requested.  Each time a filename with $num is 
+            with $num is requested.  Each time a filename with $num is
             successfully generated, the value of $num is incremented.
-    
+
             The values of variables can also be modified by a format specified
             in parentheses after the variable.  The format is simply an integer
-            that specifies how wide of a field to create for integers 
+            that specifies how wide of a field to create for integers
             (zero-padded), or, for strings, how many space separated words
             to limit the name to.  The example below shows $num being padded
             to four places and $title being limited to five words::
-    
+
                 sect$num(4).html $title(5).html
-    
-            The list can also contain a wildcard filename (which should be 
-            specified last).  Once a wildcard name is reached, it is 
-            used from that point on to generate the remaining filenames.  
+
+            The list can also contain a wildcard filename (which should be
+            specified last).  Once a wildcard name is reached, it is
+            used from that point on to generate the remaining filenames.
             The wildcard filename contains a list of alternatives to use as
-            part of the filename indicated by a comma separated list of 
+            part of the filename indicated by a comma separated list of
             alternatives surrounded by a set of square brackets ([ ]).
             Each of the alternatives specified is tried until a filename is
             successfully created (i.e. all variables resolve).  For example,
             the specification below creates three alternatives::
-     
+
                 $jobname_[$id, $title, sect$num(4)].html
-    
+
             The code above is expanded to the following possibilities::
-    
+
                 $jobname_$id.html
                 $jobname_$title.html
                 $jobname_sect$num(4).html
-    
+
             Each of the alternatives is attempted until one of them succeeds.
             Generally, the last one should contain no variables except for
             $num as a fail-safe alternative.
-    
+
         Keyword Arguments:
         charsub -- two-element list that contains character substitutions.
             The first element is a string containing all of the characters
             that are illegal in a filename.  The second string is a string
-            that will be used in place of each of the "bad" characters in 
+            that will be used in place of each of the "bad" characters in
             resulting filename.
-        vars -- the namespace of variables to use when expanding the
+        variables -- the namespace of variables to use when expanding the
             filenames.  New variables can be added to this namespace between
             each iteration.  The namespace is reset to the value sent in
             the initial generator call after each iteration.
@@ -68,14 +68,14 @@ class Filenames(object):
         invalid -- names that can not be used.  If the generated name is
             found to be one of the specified invalid names, a new one
             is generated until a valid name is found.
-    
+
         Returns:
         generator that creates filenames
-    
-        """      
+
+        """
         self.files = self.parseFilenames(spec)
         self.charsub = charsub
-        self.vars = vars
+        self.variables = variables
         self.extension = extension
         self.invalid = invalid
         self.newFilename = self._newFilename()
@@ -89,16 +89,16 @@ class Filenames(object):
         spec = re.sub(r'\[\s*', r'[', spec)
         spec = re.sub(r'\s*\]', r']', spec)
         spec = re.sub(r'\s*,\s*', r',', spec)
-    
+
         files = ['']
         spec = iter(spec)
         for char in spec:
-    
+
             # Spaces mark a division between names
             if not char.strip():
                  files.append('')
                  continue
-    
+
             # Check for alternatives
             elif char == '[':
                 options = [files[-1]]
@@ -111,16 +111,16 @@ class Filenames(object):
                     options[-1] += char
                 files[-1] = [x for x in options if x]
                 continue
-            
+
             # Append the character to the current filename
             if isinstance(files[-1], list):
                 for i, item in enumerate(files[-1]):
                     files[-1][i] += char
             else:
                 files[-1] += char
-    
+
         files = [x for x in files if x]
-    
+
         return files
 
     def __call__(self):
@@ -139,8 +139,8 @@ class Filenames(object):
 
     def _newFilename(self):
         """ Generator that generates new filenames """
-        g = self.vars.copy()
-    
+        g = self.variables.copy()
+
         # Split filenames into static and wildcard groups
         static = []
         wildcard = []
@@ -151,20 +151,20 @@ class Filenames(object):
         if self.files:
             wildcard = self.files.pop(0)
 
-        # If there is no wildcard template, assume that the last 
+        # If there is no wildcard template, assume that the last
         # template given is a wildcard template.
         if not wildcard and static:
             wildcard = [static.pop()]
-    
+
         # Initialize file number counter
         num = 1
-    
+
         # Locate all key names and formats in the string
         keysre = re.compile(r'\$\{(\w+)(?:\.(\d+))?}')
-        
+
         # Return static filenames
         for item in static:
-            currentns = self.vars.copy()
+            currentns = self.variables.copy()
             for key, value in list(currentns.items()):
                 if self.charsub:
                     for char in self.charsub[0]:
@@ -190,15 +190,15 @@ class Filenames(object):
                 result = string.Template(item).substitute(currentns)
                 if 'num' in currentns:
                     num += 1
-                self.vars.clear()
-                self.vars.update(g)
+                self.variables.clear()
+                self.variables.update(g)
                 result = self.addExtension(result)
                 if result not in self.invalid:
                     self.invalid[result] = None
                     yield result
             except KeyError as key:
                 continue
-                
+
         # We've reached the wildcard stage.  The wildcard gives us
         # multiple alternatives of filenames to choose from.  Keep trying
         # each one with the current namespace until one works.
@@ -206,7 +206,7 @@ class Filenames(object):
         while 1:
             passes += 1
             for item in wildcard:
-                currentns = self.vars.copy()
+                currentns = self.variables.copy()
                 for key, value in list(currentns.items()):
                     if self.charsub:
                         for char in self.charsub[0]:
@@ -232,8 +232,8 @@ class Filenames(object):
                     result = string.Template(item).substitute(currentns)
                     if 'num' in currentns:
                         num += 1
-                    self.vars.clear()
-                    self.vars.update(g)
+                    self.variables.clear()
+                    self.variables.update(g)
                     result = self.addExtension(result)
                     if result not in self.invalid:
                         self.invalid[result] = None
@@ -242,13 +242,13 @@ class Filenames(object):
                         continue
                     break
                 except KeyError as key:
-                    if 'num' in self.vars:
-                        del self.vars['num']
+                    if 'num' in self.variables:
+                        del self.variables['num']
                     continue
             else:
                 # We tried 100 different names and still don't have a unique
                 # one, we better just bail out.
                 if passes > 100:
                     break
-    
+
         raise ValueError('Filename could not be created.')

commit f4a7c6ba785e450c8a16e1713d9b3fbd751acb03

Fix #113
Reverts an accidental regression from 2decbb1. See the linked issue for
more details.

plasTeX/TeX.py
@@ -134,8 +134,7 @@ class TeX(object):
                     encoding = 'utf_8_sig'
 
                 fname = self.kpsewhich(myfile)
-                with open(fname, encoding=encoding) as fd:
-                    self.input(fd.read())
+                self.input(open(fname, encoding=encoding))
                 self.jobname = os.path.basename(os.path.splitext(myfile)[0])
 
             # File object