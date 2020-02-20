edc163d37296bebc5fead6d9ace2228440ea3489
N. Raghavendra <raghu@hri.res.in>
2020-02-20 21:05:17 +0530

    Add the packageprefix package
    * plasTeX/Packages/packageprefix.py: New file.  Contains the
      definition of the \addpackageprefix macro for associating a prefix
      with a package.  Macros in that package can then be used with the
      prefix.  This introduces a rudimentary namespace facility for
      macros.

---
+++ plasTeX/Packages/packageprefix.py
@@ -0,0 +1,48 @@
+import importlib
+from plasTeX import Command
+from plasTeX.Logging import getLogger
+
+log = getLogger()
+
+
+class PackagePrefixAdder(Command):
+    macroName = 'addpackageprefix'
+    args = 'prefix:str package:str'
+
+    def invoke(self, tex):
+        super().invoke(tex)
+        attributes = self.attributes
+        prefix = attributes['prefix']
+        package = attributes['package']
+        packages = self.ownerDocument.context.packages
+
+        if package in packages:
+            module = importlib.import_module(package)
+            exports = getattr(module, 'package_prefix_exports', None)
+            if exports is not None:
+                for export in exports:
+                    copy_name = prefix + export
+                    main_name = 'Main_%s_%s' % (package, export)
+                    main_class = getattr(module, main_name, None)
+                    if main_class is not None:
+                        copy_class = type(
+                            copy_name, (main_class,), {}
+                        )
+                        self.ownerDocument.context.addGlobal(
+                            copy_name, copy_class
+                        )
+                    else:
+                        log.info(
+                            'The export %s does not have a'
+                            ' definition %s' % (export, main_name)
+                        )
+            else:
+                log.info(
+                    'The package %s does not define any exports'
+                    ' to be prefixed' % package
+                )
+        else:
+            log.info(
+                'The package %s has not been loaded;'
+                ' nothing to be prefixed' % package
+            )
