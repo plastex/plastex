36dd52bed7f9634267697f1eeb7da39aa54499a2
Dexter Chua <dalcde@yahoo.com.hk>
2020-03-29 09:34:34 +0800

    Add pdf2svg imager
    Currently this uses pdfcrop to crop the pdf before converting it to svg.
    I've been unable to find a straightforward way to crop svg's; something
    we should probably look into in the future (the Image.crop function
    doesn't work on svg's. We should fix that).

---
+++ plasTeX/Imagers/pdf2svg.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import os
+import subprocess
+from plasTeX.Imagers import VectorImager
+
+class PDFSVG(VectorImager):
+    """ Imager that uses pdf2svg """
+    fileExtension = '.svg'
+    verifications = ['pdflatex --help', 'which pdf2svg', 'pdfcrop --help']
+    compiler = 'pdflatex'
+
+    def executeConverter(self, output):
+        open('images-uncropped.pdf', 'wb').write(output.read())
+        subprocess.call(["pdfcrop", "images-uncropped.pdf", "images.pdf"], stdout=subprocess.PIPE)
+
+        rc = 0
+        page = 1
+        while 1:
+            filename = 'img%d.svg' % page
+            rc = subprocess.call(['pdf2svg', 'images.pdf', filename, str(page)], stdout=subprocess.PIPE)
+            if rc:
+                break
+
+            if not open(filename).read().strip():
+                os.remove(filename)
+                break
+            page += 1
+            if page > len(self.images):
+                break
+        return rc, None
+
+Imager = PDFSVG
