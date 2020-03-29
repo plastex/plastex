94f7c66b32ff205b479cfb7d2f51a19d2310e1f1
Dexter Chua <dalcde@yahoo.com.hk>
2020-03-29 13:09:08 +0800

    Let pdf2svg Imager respect scale factor

--- plasTeX/Imagers/pdf2svg.py
+++ plasTeX/Imagers/pdf2svg.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
-import os
 import subprocess
+import re
+import xml.etree.ElementTree as ET
 from plasTeX.Imagers import VectorImager
 
+length_re = re.compile('([0-9\\.]*)(.*)')
 class PDFSVG(VectorImager):
     """ Imager that uses pdf2svg """
     fileExtension = '.svg'
@@ -12,19 +14,30 @@ class PDFSVG(VectorImager):
 
     def executeConverter(self, output):
         open('images-uncropped.pdf', 'wb').write(output)
-        subprocess.call(["pdfcrop", "images-uncropped.pdf", "images.pdf"], stdout=subprocess.PIPE)
+        subprocess.call(["pdfcrop", "images-uncropped.pdf", "images.pdf"], stdout=subprocess.DEVNULL)
 
         rc = 0
         page = 1
         while 1:
             filename = 'img%d.svg' % page
-            rc = subprocess.call(['pdf2svg', 'images.pdf', filename, str(page)], stdout=subprocess.PIPE)
+            rc = subprocess.call(['pdf2svg', 'images.pdf', filename, str(page)], stdout=subprocess.DEVNULL)
             if rc:
                 break
 
-            if not open(filename).read().strip():
-                os.remove(filename)
-                break
+            scale = self.config["images"]["scale-factor"]
+            if scale != 1:
+                tree = ET.parse(filename)
+                root = tree.getroot()
+
+                width = length_re.match(root.attrib["width"])
+                root.attrib["width"] = "{}{}".format(float(width.group(1)) * scale, width.group(2))
+
+                height = length_re.match(root.attrib["height"])
+                root.attrib["height"] = "{}{}".format(float(height.group(1)) * scale, height.group(2))
+
+                tree.write(filename)
+
+
             page += 1
             if page > len(self.images):
                 break
