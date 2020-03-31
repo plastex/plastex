d343ca183d7ef67cbe14cdc61f2361b1abd0bfe7
Dexter Chua <dalcde@yahoo.com.hk>
2020-03-31 19:27:03 +0800

    Write better code in pdf2svg

--- plasTeX/Imagers/pdf2svg.py
+++ plasTeX/Imagers/pdf2svg.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from pathlib import Path
 import subprocess
 import re
 import xml.etree.ElementTree as ET
@@ -13,12 +14,12 @@ class PDFSVG(VectorImager):
     compiler = 'pdflatex'
 
     def executeConverter(self, output):
-        open('images-uncropped.pdf', 'wb').write(output)
+        Path("images-uncropped.pdf").write_bytes(output)
+
         subprocess.call(["pdfcrop", "images-uncropped.pdf", "images.pdf"], stdout=subprocess.DEVNULL)
 
         rc = 0
-        page = 1
-        while 1:
+        for page in range(1, len(self.images) + 1):
             filename = 'img%d.svg' % page
             rc = subprocess.call(['pdf2svg', 'images.pdf', filename, str(page)], stdout=subprocess.DEVNULL)
             if rc:
@@ -29,18 +30,12 @@ class PDFSVG(VectorImager):
                 tree = ET.parse(filename)
                 root = tree.getroot()
 
-                width = length_re.match(root.attrib["width"])
-                root.attrib["width"] = "{}{}".format(float(width.group(1)) * scale, width.group(2))
-
-                height = length_re.match(root.attrib["height"])
-                root.attrib["height"] = "{}{}".format(float(height.group(1)) * scale, height.group(2))
+                for attrib in ["width", "height"]:
+                    m = length_re.match(root.attrib[attrib])
+                    root.attrib[attrib] = "{}{}".format(float(m.group(1)) * scale, m.group(2))
 
                 tree.write(filename)
 
-
-            page += 1
-            if page > len(self.images):
-                break
         return rc, None
 
 Imager = PDFSVG
