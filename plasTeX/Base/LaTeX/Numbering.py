7a5467e9790ce70c1c7fdbe80484a8b3b0da1955
Dexter Chua <dalcde@yahoo.com.hk>
2020-04-20 20:14:39 +0800

    Add \romannumeral command

--- plasTeX/Base/LaTeX/Numbering.py
+++ plasTeX/Base/LaTeX/Numbering.py
@@ -44,46 +44,7 @@ class Roman(Command):
     args = 'name:str'
     def invoke(self, tex):
         a = self.parse(tex)
-        roman = ""
-        n, number = divmod(int(self.ownerDocument.context.counters['name']), 1000)
-        roman = "M"*n
-        if number >= 900:
-            roman = roman + "CM"
-            number = number - 900
-        while number >= 500:
-            roman = roman + "D"
-            number = number - 500
-        if number >= 400:
-            roman = roman + "CD"
-            number = number - 400
-        while number >= 100:
-            roman = roman + "C"
-            number = number - 100
-        if number >= 90:
-            roman = roman + "XC"
-            number = number - 90
-        while number >= 50:
-            roman = roman + "L"
-            number = number - 50
-        if number >= 40:
-            roman = roman + "XL"
-            number = number - 40
-        while number >= 10:
-            roman = roman + "X"
-            number = number - 10
-        if number >= 9:
-            roman = roman + "IX"
-            number = number - 9
-        while number >= 5:
-            roman = roman + "V"
-            number = number - 5
-        if number >= 4:
-            roman = roman + "IV"
-            number = number - 4
-        while number > 0:
-            roman = roman + "I"
-            number = number - 1
-        return tex.textTokens(roman)
+        return tex.textTokens(self.ownerDocument.context.counters[a['name']].Roman)
 
 class roman(Roman):
     """ Return the lowercase roman representation """
