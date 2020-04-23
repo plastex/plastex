plastex/plastex

commit fbb79a55334d07746b21d1f0c64cf54d1fd731f1

Fix `Macro.source` for non-letter commands
If a command name comprises of a single non-letter, then there should
not be a space between the command and the argument because the command
is necessarily one character long.

One can test this with $\text{\"o}$ using the HTML5 renderer

plasTeX/__init__.py
@@ -421,7 +421,8 @@ class Macro(Element):
         argSource = sourceArguments(self)
         if not argSource:
             argSource = ' '
-        elif argSource[0] in encoding.stringletters():
+        elif argSource[0] in encoding.stringletters() and\
+             not (len(self.macroName) == 1 and self.macroName[0] not in encoding.stringletters()):
             argSource = ' %s' % argSource
         s = '%s%s%s' % (escape, name, argSource)
 

commit 61e25c68910bd4124f2a3913c3bf81a0e6cf9b61

Fix regression caused by fbb79a5. Fixes #192
macroName is not always defined; we should use nodeName which defaults
to the class name when macroName is not defined.

plasTeX/__init__.py
@@ -422,7 +422,7 @@ class Macro(Element):
         if not argSource:
             argSource = ' '
         elif argSource[0] in encoding.stringletters() and\
-             not (len(self.macroName) == 1 and self.macroName[0] not in encoding.stringletters()):
+             not (len(name) == 1 and name[0] not in encoding.stringletters()):
             argSource = ' %s' % argSource
         s = '%s%s%s' % (escape, name, argSource)
 

unittests/Source.py
@@ -32,6 +32,14 @@ class Source(TestCase):
         source = normalize(output.source)
         assert input == source, '"%s" != "%s"' % (input, source)
 
+    def testMathCal(self):
+        input = r'a $ \mathcal A $ b'
+        s = TeX()
+        s.input(input)
+        output = s.parse()
+        source = normalize(output.source)
+        assert input == source, '"%s" != "%s"' % (input, source)
+
     def testDisplayMath(self):
         input = r'a \[ x^{y_3} \]b'
         s = TeX()