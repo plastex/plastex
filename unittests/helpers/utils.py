901b967bf67e0c6644b5d63d65b89348a70787a9
Dexter Chua <dalcde@yahoo.com.hk>
2020-04-23 20:25:27 +0800

    Add helper utility to check TeX output against plasTeX

--- unittests/helpers/utils.py
+++ unittests/helpers/utils.py
@@ -1,5 +1,8 @@
 import subprocess
 import re
+import os
+from plasTeX.TeX import TeX
+from tempfile import TemporaryDirectory
 
 def cmp_img(a: str, b: str) -> float:
     out = subprocess.run(["compare", "-quiet", "-metric", "MSE",
@@ -30,3 +33,32 @@ def cmp_img(a: str, b: str) -> float:
 
     # out.stderr is a byte sequence, but float can take byte sequences
     return float(diff)
+
+# This checks whether plastex produces the same output as tex. This only works
+# for things with simple plain text output, and is intended to be used for
+# testing primitives such as \let, \def, \expandafter etc.
+def compare_output(tex: str):
+    cwd = os.getcwd()
+    try:
+        plastex_out = TeX().input(tex).parse().textContent
+        with TemporaryDirectory() as tmpdir:
+            os.chdir(tmpdir)
+
+            if r'\documentclass{article}' not in tex:
+                tex = r'\documentclass{article}\begin{document}' + tex + r'\end{document}'
+            tex = r'\nonstopmode\AtBeginDocument{\thispagestyle{empty}}' + tex
+
+            with open("test.tex", "w") as f:
+                f.write(tex)
+
+            subprocess.run(["pdflatex", "test.tex"], check=True)
+            out = subprocess.run(
+                    ["gs", "-q", "-sDEVICE=txtwrite", "-o", "%stdout%", "test.pdf"],
+                    check=True,
+                    stdout=subprocess.PIPE,
+                    stdin=subprocess.DEVNULL,
+                    universal_newlines=True)
+            tex_out = out.stdout
+        assert plastex_out.strip() == tex_out.strip()
+    finally:
+        os.chdir(cwd)
