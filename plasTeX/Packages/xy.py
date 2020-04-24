f4e5cc3b286503752f2116bc3a52a5763372ed75
Dexter Chua <dalcde@yahoo.com.hk>
2020-04-24 18:08:13 +0800

    Copy xymatrix implementation from gerby.
    I have not verified the implementation but added tests to check that
    source returns the right result.

---
+++ plasTeX/Packages/xy.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+
+from plasTeX import Command
+
+class xymatrix(Command):
+    args = '{ str }'
+    doCharSubs = False
+
+    class EndRow(Command):
+        """ End of a row """
+        macroName = '\\'
+
+    class CellDelimiter(Command):
+        """ Cell delimiter """
+        macroName = 'active::&'
+        pass
+
+    class ar(Command):
+        args = '[ misc:str ]'
+
+    class arAt(Command):
+        macroName = "ar@"
+        args = 'arType:str [ misc:str ]'
+
+        def postArgument(self, arg, value, tex):
+            self.argSource = self.argSource.replace("~ ","~")
+            super(Command, self).postArgument(arg, value, tex)
+
+    class omit(Command):
+        @property
+        def source(self):
+            return '\omit'
+
+    def preArgument(self, arg, tex):
+            # Check whether there are any spacing arguments for xymatrix
+            self.spacingArgs = []
+            for t in tex.itertokens():
+                if t == '{':
+                    tex.pushToken(t)
+                    break
+                else:
+                    self.spacingArgs.append(t)
+            super(xymatrix, self).preArgument(arg, tex)
+
+    def postArgument(self, arg, value, tex):
+            self.argSource = "".join(self.spacingArgs) + self.argSource
+            super(xymatrix, self).postArgument(arg, value, tex)
+
+# These are variants of the xymatrix command in which some spacing parameters
+# are specified before the main argument.
+# See section 3.3 of the "XY-pic User's Guide'.
+class xymatrixR(xymatrix):
+    macroName = "xymatrix@R"
+
+class xymatrixC(xymatrix):
+    macroName = "xymatrix@C"
+
+class xymatrixM(xymatrix):
+    macroName = "xymatrix@M"
+
+class xymatrixW(xymatrix):
+    macroName = "xymatrix@W"
+
+class xymatrixH(xymatrix):
+    macroName = "xymatrix@H"
+
+class xymatrixL(xymatrix):
+    macroName = "xymatrix@L"
+
+class xymatrix1(xymatrix):
+    macroName = "xymatrix@1"
+
+class xyoption(Command):
+    # Options for xymatrix; unless we are writing xymatrix completely in python,
+    # this is mostly irrelevant.
+    pass
+
+class useTwocells(Command):
+    macroName ="UseTwocells"
+
+class useAllTwocells(Command):
+    macroName = "UseAllTwocells"
--- unittests/Source.py
+++ unittests/Source.py
@@ -10,6 +10,29 @@ def normalize(s):
 
 
 class Source(TestCase):
+    def testXYMatrix(self):
+        input = r'\xymatrix{A \ar[d]^b \ar[r]^a &B\ar[d]^c\\ C \ar[r]^d &D}'
+        s = TeX()
+        s.input(input)
+        output = s.parse()
+        source = normalize(output.source)
+        assert input.replace(" ", "") == source.replace(" ", ""), '"%s" != "%s"' % (input, source)
+
+    def testXYMatrix2(self):
+        input = r'\xymatrix{A \ar@{->>}[rd] \ar@{^{(}->}[r]&B \ar@{.>}[d]&C \ar@{_{(}->}[l]\ar@{->>}[ld]\\&D}'
+        s = TeX()
+        s.input(input)
+        output = s.parse()
+        source = normalize(output.source)
+        assert input.replace(" ", "") == source.replace(" ", ""), '"%s" != "%s"' % (input, source)
+
+    def testXYMatrix3(self):
+        input = r'\xymatrix{A \ar[r]^u_d \ar[rd]^u_d &B \ar[d]^u_d &C \ar[l]^u_d \ar[ld]^u_d\\&D}'
+        s = TeX()
+        s.input(input)
+        output = s.parse()
+        source = normalize(output.source)
+        assert input.replace(" ", "") == source.replace(" ", ""), '"%s" != "%s"' % (input, source)
 
     def testList(self):
         input = r'\begin{enumerate} \item one \item two \item three \end{enumerate}'
