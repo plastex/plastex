88b2d0f0e81cd734664756c56a89ea75da35024d
Dexter Chua <dalcde@yahoo.com.hk>
2020-05-04 16:19:02 +0800

    Add some tikz commands that do nothing

--- plasTeX/Packages/tikz.py
+++ plasTeX/Packages/tikz.py
@@ -2,6 +2,13 @@
 Implement the tikz package using the imager
 """
 from plasTeX import VerbatimEnvironment
+from plasTeX import Command
 
 class tikzpicture(VerbatimEnvironment):
     pass
+
+class usetikzlibrary(Command):
+    args = "library"
+
+class tikzset(Command):
+    args = "library"
