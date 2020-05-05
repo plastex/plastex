979c122eaf58c13ffb832caefc7bf21f719331f2
Dexter Chua <dalcde@yahoo.com.hk>
2020-05-05 14:01:57 +0800

    Add basic todonotes implementation

---
+++ plasTeX/Packages/todonotes.py
@@ -0,0 +1,18 @@
+from plasTeX import Command
+
+class todo(Command):
+    disabled = False
+    args = '[ attr:dict ] todo'
+
+    @property
+    def blockType(self):
+        if self.attributes.get("attr") is None:
+            return False
+
+        return bool(self.attributes["attr"].get("inline"))
+
+def ProcessOptions(options, document):
+    if options.get("disable"):
+        document.todoNotesDisable = True
+    else:
+        document.todoNotesDisable = False
---
+++ unittests/Packages/TodoNotes.py
@@ -0,0 +1,46 @@
+from plasTeX.TeX import TeX, TeXDocument
+from plasTeX.Renderers import Renderer
+from pathlib import Path
+
+def test_todo():
+    todo = TeX().input(r'\usepackage{todonotes}\todo{123}').parse()[1]
+    assert not todo.blockType
+    assert todo.attributes["todo"].textContent == "123"
+
+def test_todo_inline():
+    todo = TeX().input(r'\usepackage{todonotes}\todo[inline]{123}').parse()[1]
+    assert todo.blockType
+    assert todo.attributes["todo"].textContent == "123"
+
+def test_todo_disable(tmpdir):
+    renderer = Renderer()
+    def render_todo(obj):
+        if obj.ownerDocument.todoNotesDisable:
+            return "disabled"
+        else:
+            return str(obj.attributes["todo"])
+
+    renderer["todo"] = render_todo
+
+    enabled = r'''
+\documentclass{article}
+\usepackage{todonotes}
+\begin{document}
+  \todo{foo}
+\end{document}
+'''
+
+    disabled = r'''
+\documentclass{article}
+\usepackage[disable]{todonotes}
+\begin{document}
+  \todo{foo}
+\end{document}
+'''
+
+    with tmpdir.as_cwd():
+        renderer.render(TeX().input(enabled).parse())
+        assert Path("index").read_text().strip() == "foo"
+
+        renderer.render(TeX().input(disabled).parse())
+        assert Path("index").read_text().strip() == "disabled"
