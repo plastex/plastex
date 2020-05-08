40deae035a1ce5bcb4adca926067fa725c4b9e64
Dexter Chua <dalcde@yahoo.com.hk>
2020-05-08 18:55:11 +0800

    Stop ConfigFileReading from polluting repository
    Currently running pytest creates `empty_article/` and
    `empty_article.paux` in cwd.

--- unittests/ConfigFileReading.py
+++ unittests/ConfigFileReading.py
@@ -5,15 +5,16 @@ import subprocess
 config_path = str((Path(__file__).parent/'sample_config').absolute())
 src_path = str((Path(__file__).parent/'empty_article.tex').absolute())
 
-def test_config_file_reading():
+def test_config_file_reading(tmpdir):
     """
     Check that config file provided on command line are read.
     This is a cli only test, hence difficult to test directly.
     Here we use a config file asking for a non-existant theme, then
     check a warning appears on stderr.
     """
-    out = subprocess.run(['plastex', '-c', config_path, src_path], check=True,
-                         stderr=subprocess.PIPE).stderr.decode()
-    assert 'WARNING: Using default renderer for document-layout' in out
+    with tmpdir.as_cwd():
+        out = subprocess.run(['plastex', '-c', config_path, src_path], check=True,
+                             stderr=subprocess.PIPE).stderr.decode()
+        assert 'WARNING: Using default renderer for document-layout' in out
 
 
