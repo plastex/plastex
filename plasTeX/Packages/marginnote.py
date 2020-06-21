896dcdd451a885a4bddf6ca4e0f0bcf80d8eea9d
Patrick Massot <patrickmassot@free.fr>
2020-06-21 16:53:10 +0200

    marginnote stub

---
+++ plasTeX/Packages/marginnote.py
@@ -0,0 +1,11 @@
+"""
+marginnote package stub.
+
+There is no actual functionality here, but it does allow defining a marginnote
+template.
+"""
+
+from plasTeX import Command
+
+class marginnote(Command):
+    args = 'note'
