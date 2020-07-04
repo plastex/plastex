4694a6f83e2001970f47f2a2c9c87928bc0dfb22
N. Raghavendra <raghu@hri.res.in>
2020-07-04 07:50:57 +0530

    Make the installation Nix expression a proper package
    * misc/install.nix: Make the expression a proper package derivation.
      Specify dependencies as arguments.  Provide package metadata.

--- misc/install.nix
+++ misc/install.nix
@@ -1,12 +1,31 @@
 ### Install the project in development mode
 
-let
-  dependencies = import ./misc/dependencies.nix;
-in dependencies.buildPythonPackage {
+{
+  stdenv, buildPythonPackage, cleanSource, certifi, chardet, docopt,
+  jinja2, unidecode, jinja2-ansible-filters, dvipng
+}:
+
+buildPythonPackage {
   name = "plastex";
-  src = ./..;
+  src = cleanSource ./..;
   doCheck = false;
-  propagatedBuildInputs = dependencies.packages;
+  propagatedBuildInputs = [
+    certifi chardet docopt jinja2 unidecode jinja2-ansible-filters
+    dvipng
+  ];
+  meta = {
+    description = "Convert LaTeX documents to various formats";
+    homepage = "https://github.com/nyraghu/plastex";
+    license = [ stdenv.lib.licenses.mit ];
+    maintainers = [
+      {
+        email = "raghu@hri.res.in";
+        github = "nyraghu";
+        githubId = 11349287;
+        name = "N. Raghavendra";
+      }
+    ];
+  };
 }
 
 ### End of file
