dcbeb8998aa7268ffd3f73915a6cb7c5c2acf2e0
N. Raghavendra <raghu@hri.res.in>
2020-07-05 16:28:21 +0530

    Clean Nix dependencies and distributions files
    * misc/distributions.nix: Move all stuff that is not related to source
      distributions to the dependencies file.
    
    * misc/dependencies.nix: Modify to accommodate the above change.

--- misc/dependencies.nix
+++ misc/dependencies.nix
@@ -2,7 +2,11 @@
 
 let
   distributions = import ./distributions.nix;
-  python = distributions.python;
+  nixpkgs = import distributions.nixpkgsPath {};
+  python = nixpkgs.python38;
+  nixPython = import distributions.nixPythonPath {
+    inherit nixpkgs python;
+  };
   pythonEnvironment = python.withPackages (
     packages:
     [
@@ -14,23 +18,23 @@ let
       packages.pycodestyle
       packages.setuptools
       packages.unidecode
-      distributions.nixPython.jinja2-ansible-filters
+      nixPython.jinja2-ansible-filters
     ]
   );
-  nixpkgs = distributions.nixpkgs;
   texlive = nixpkgs.texlive;
-  texLiveEnvironment = texlive.combine {
+  texliveEnvironment = texlive.combine {
     inherit (texlive) scheme-infraonly dvipng;
   };
-  nodeJsPackages = with distributions; [
+  nixNodeJs = import distributions.nixNodeJsPath { pkgs = nixpkgs; };
+  nodeJsPackages = [
     nixNodeJs."css-validator-0.9.0"
     nixNodeJs."sass-1.26.7"
   ];
 in {
   inherit (nixpkgs) buildEnv mkShell;
   inherit (python.pkgs) buildPythonPackage;
-  packages = [ pythonEnvironment texLiveEnvironment ]
+  packages = [ pythonEnvironment texliveEnvironment ]
              ++ nodeJsPackages;
 }
-
+  b
 ### End of file
