8648fa13150b0794b41b6573325cccf418a4ece3
Patrick Massot <patrickmassot@free.fr>
2020-12-27 16:29:32 +0100

    Change test style in CSSStyles

--- unittests/CSSStyles.py
+++ unittests/CSSStyles.py
@@ -1,63 +1,35 @@
-#!/usr/bin/env python
+from pytest import fixture
 
-import unittest, re
-from unittest import TestCase
 from plasTeX.TeX import TeX
-from plasTeX import Macro
 
-class InlineStyle(TestCase):
+@fixture
+def node():
+    s = TeX()
+    s.input(r'\texttt{hi}')
+    return s.parse().childNodes[0]
 
-    def testNoStyle(self):
-        input = '\\texttt{hi}'
-        s = TeX()
-        s.input(input)
-        output = s.parse()
-        node = output.childNodes[0]
-        style = node.style.inline
-        assert style is None
+def test_no_style(node):
+    style = node.style.inline
+    assert style is None
 
-    def testEmptyStyle(self):
-        input = '\\texttt{hi}'
-        s = TeX()
-        s.input(input)
-        output = s.parse()
-        node = output.childNodes[0]
-        node.style['width'] = ''
-        style = node.style.inline
-        assert style is None
+def test_empty_style(node):
+    node.style['width'] = ''
+    style = node.style.inline
+    assert style is None
 
-    def testOneStyle(self):
-        input = '\\texttt{hi}'
-        s = TeX()
-        s.input(input)
-        output = s.parse()
-        node = output.childNodes[0]
-        node.style['width'] = '100%'
-        style = node.style.inline
-        assert style == 'width:100%'
+def test_one_style(node):
+    node.style['width'] = '100%'
+    style = node.style.inline
+    assert style == 'width:100%'
 
-    def testTwoStyles(self):
-        input = '\\texttt{hi}'
-        s = TeX()
-        s.input(input)
-        output = s.parse()
-        node = output.childNodes[0]
-        node.style['width'] = '100%'
-        node.style['height'] = '100%'
-        style = node.style.inline
-        assert style == 'width:100%; height:100%'
+def test_two_styles(node):
+    node.style['width'] = '100%'
+    node.style['height'] = '100%'
+    style = node.style.inline
+    assert style == 'width:100%; height:100%'
 
-    def testEmptyStyleExcluded(self):
-        input = '\\texttt{hi}'
-        s = TeX()
-        s.input(input)
-        output = s.parse()
-        node = output.childNodes[0]
-        node.style['width'] = ''
-        node.style['height'] = '100%'
-        style = node.style.inline
-        assert style == 'height:100%'
-
-if __name__ == '__main__':
-    unittest.main()
-
+def test_empty_style_excluded(node):
+    node.style['width'] = ''
+    node.style['height'] = '100%'
+    style = node.style.inline
+    assert style == 'height:100%'
