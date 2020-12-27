plastex/plastex

commit 4f1dbedab1fbc653aceed39dc62a557224e7eb3c

Allow empty js folder in HTML5 theme

plasTeX/Renderers/HTML5/__init__.py
@@ -57,10 +57,12 @@ class HTML5(_Renderer):
             pass
 
         # Start building the js list for use by the layout template
+        theme_js_path = Path(self.loadedTheme)/'js'
         if (config['html5']['use-theme-js'] and
-                config['general']['copy-theme-extras']):
+                config['general']['copy-theme-extras'] and
+                theme_js_path.exists()):
             rendererdata['js'] = sorted(
-                    os.listdir(os.path.join(self.loadedTheme, 'js')))
+                    str(path) for path in theme_js_path.glob('*.js'))
         else:
             rendererdata['js'] = []
 

commit 69237aeeb6955e65ddd1da9951ac970dbbc0bcd5

Fix js file name bug introduced in 4f1dbedab

plasTeX/Renderers/HTML5/__init__.py
@@ -61,8 +61,8 @@ class HTML5(_Renderer):
         if (config['html5']['use-theme-js'] and
                 config['general']['copy-theme-extras'] and
                 theme_js_path.exists()):
-            rendererdata['js'] = sorted(
-                    str(path) for path in theme_js_path.glob('*.js'))
+            rendererdata['js'] = sorted(path.name
+                    for path in theme_js_path.glob('*.js'))
         else:
             rendererdata['js'] = []
 