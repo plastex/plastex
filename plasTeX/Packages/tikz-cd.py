ea53aa22c28ab4f7ded991c359dd4811a885c647
Patrick Massot <patrickmassot@free.fr>
2021-01-23 22:06:00 +0100

    Add missing command in tikz-cd

--- plasTeX/Packages/tikz-cd.py
+++ plasTeX/Packages/tikz-cd.py
@@ -1,7 +1,13 @@
-from plasTeX import VerbatimEnvironment
+from plasTeX import VerbatimEnvironment, Command
 
 def ProcessOptions(options, document):
     document.config['images']['scales'].setdefault('tikzcd', 1.5)
 
 class tikzcd(VerbatimEnvironment):
     pass
+
+class usetikzlibrary(Command):
+    args = "library"
+
+class tikzset(Command):
+    args = "library"
