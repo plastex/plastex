9cd40c4168e822898167c0afc3f701183ab2638b
Patrick Massot <patrickmassot@free.fr>
2021-03-28 18:30:02 +0200

    Information when loading amsart and amsbook

--- plasTeX/Packages/amsart.py
+++ plasTeX/Packages/amsart.py
@@ -1,1 +1,7 @@
-from plasTeX.Packages.article import *
+from plasTeX import log
+from plasTeX.Packages.article import appendix
+import plasTeX.Packages.article as article
+
+def ProcessOptions(options, document): # type: ignore
+    log.info('The amsart class is currently nothing but the article class.')
+    article.ProcessOptions(options, document)
--- plasTeX/Packages/amsbook.py
+++ plasTeX/Packages/amsbook.py
@@ -1,3 +1,8 @@
-#!/usr/bin/env python
+from plasTeX import log
 
 from plasTeX.Packages.book import *
+import plasTeX.Packages.book as book
+
+def ProcessOptions(options, document): # type: ignore
+    log.info('The amsbook class is currently nothing but the book class.')
+    book.ProcessOptions(options, document)
