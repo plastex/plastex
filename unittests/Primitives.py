7b7ba995ded6aba91d8e8c137bc25a7fb0ac5f8d
Efim Mazhnik <efimmazhnik@gmail.com>
2021-07-02 21:38:19 +0300

    fix double dollar construction rendering

--- unittests/Primitives.py
+++ unittests/Primitives.py
@@ -1,3 +1,4 @@
+from plasTeX.TeX import TeX
 from helpers.utils import compare_output
 
 def test_expandafter():
@@ -17,3 +18,35 @@ def test_expandafter_once():
 
 def test_expandafter_non_macro():
     compare_output(r'\def\foo#1{#1.}\expandafter\foo ab')
+
+def test_evil_double_dollars():
+    tex = TeX()
+    tex.input('$a$$b$')
+    nodes = tex.parse().getElementsByTagName('math')
+    assert len(nodes) == 2
+    assert nodes[0].textContent == 'a'
+    assert nodes[1].textContent == 'b'
+
+def test_more_evil_double_dollars():
+    tex = TeX()
+    tex.input('$a$$$b$$$c$')
+    doc = tex.parse()
+    mathnodes = doc.getElementsByTagName('math')
+    assert len(mathnodes) == 2
+    assert mathnodes[0].textContent == 'a'
+    assert mathnodes[1].textContent == 'c'
+    displaymathnodes = doc.getElementsByTagName('displaymath')
+    assert len(displaymathnodes) == 1
+    assert displaymathnodes[0].textContent == 'b'
+
+def test_even_more_evil_double_dollars():
+    tex = TeX()
+    tex.input('$a$$$$$$$b$$')
+    doc = tex.parse()
+    mathnodes = doc.getElementsByTagName('math')
+    assert len(mathnodes) == 1
+    assert mathnodes[0].textContent == 'a'
+    displaymathnodes = doc.getElementsByTagName('displaymath')
+    assert len(displaymathnodes) == 2
+    assert displaymathnodes[0].textContent == ''
+    assert displaymathnodes[1].textContent == 'b'
