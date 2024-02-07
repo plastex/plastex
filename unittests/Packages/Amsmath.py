5a7c804ccb8c0560b37ae9c9f268b06f5f14e621
Christian Lawson-Perfect <christianperfect@gmail.com>
2024-02-07 13:25:36 +0000

    add a unit test for \imath and \jmath

--- unittests/Packages/Amsmath.py
+++ unittests/Packages/Amsmath.py
@@ -1,6 +1,6 @@
 from plasTeX.TeX import TeX
 
-DOC = r"""
+DOC_numberwithin = r"""
 \documentclass{article}
 \usepackage{amsmath}
 \numberwithin{equation}{section}
@@ -22,9 +22,32 @@ e = f
 
 def test_numberwithin():
     s = TeX()
-    s.input(DOC)
+    s.input(DOC_numberwithin)
     output = s.parse()
     equations =output.getElementsByTagName('equation')
     assert equations[0].ref.textContent == '1.1'
     assert equations[1].ref.textContent == '2.1'
     assert equations[2].ref.textContent == '2.2'
+
+DOC_imath = r"""
+\documentclass{article}
+
+\begin{document}
+
+$\imath$ $\jmath$
+
+\end{document}
+"""
+
+def test_imath():
+    s = TeX()
+    s.input(DOC_imath)
+    output = s.parse()
+
+    imath = output.getElementsByTagName('imath')[0]
+    assert len(imath.childNodes) == 0
+    assert imath.str == chr(305)
+    jmath = output.getElementsByTagName('jmath')[0]
+
+    assert len(jmath.childNodes) == 0
+    assert jmath.str == chr(567)
