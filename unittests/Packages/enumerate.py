4a8c5981884e4233717ba6dcd73be098520b1fea
Christian Lawson-Perfect <christianperfect@gmail.com>
2024-04-02 12:39:28 +0100

    \item custom labels: escape HTML
    This runs labels for list items through jinja's escape filter, replacing
    for example `>` with `&gt;`

--- unittests/Packages/enumerate.py
+++ unittests/Packages/enumerate.py
@@ -1,3 +1,32 @@
+from plasTeX.TeX import TeX
+
+def test_enumerate():
+    tex = TeX()
+    tex.input(r'''
+      \documentclass{article}
+      \begin{document}
+      \begin{enumerate}
+        \item First
+        \item Second
+      \end{enumerate}
+
+      \begin{enumerate}
+        ignore this
+        \item First
+        \vspace{1em} % also ignored
+        \item Second
+      \end{enumerate}
+
+      \end{document}
+      ''')
+
+    enums = tex.parse().getElementsByTagName('enumerate')
+
+    items = enums[0].getElementsByTagName('item')
+    assert len(items) == 2
+
+    items = enums[1].getElementsByTagName('item')
+    assert len(items) == 2
 import pytest
 from plasTeX.TeX import TeX
 
