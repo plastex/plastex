6cf92b478297ab805367ce32cad83af8fada6434
Christian Lawson-Perfect <christianperfect@gmail.com>
2024-07-26 09:42:25 +0100

    add some documentation to dsfont.py

--- plasTeX/Packages/dsfont.py
+++ plasTeX/Packages/dsfont.py
@@ -1,3 +1,11 @@
+"""
+The LaTeX package doublestroke (imported as dsfont) supplies a font of double-struck characters.
+
+This implements the package in plasTeX by rewriting \mathds{.} to \mathbb{.}, so MathJax will use doublestruck characters from its fonts.
+
+MathJax v4 will add proper support for dsfont: https://github.com/mathjax/MathJax-src/pull/1057
+"""
+
 from plasTeX import Command, sourceArguments
 
 
