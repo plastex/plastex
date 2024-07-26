b39693b85e5c2f27786858aed9773ae01a8e6dca
Christian Lawson-Perfect <christianperfect@gmail.com>
2024-07-26 10:42:35 +0100

    add documentation to the top of cancel.py

--- plasTeX/Packages/cancel.py
+++ plasTeX/Packages/cancel.py
@@ -1,3 +1,11 @@
+"""
+Implement the commands from the LaTeX ``cancel`` package: https://ctan.org/pkg/cancel
+
+In math mode, MathJax implements these commands so they're passed through unmodified.
+
+In text mode, the HTML5 renderer renders these as ``<del>`` elements.
+"""
+
 from plasTeX import Command
 
 
