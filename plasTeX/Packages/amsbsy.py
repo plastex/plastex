fa32b7ccb95b7ef2537450b168f3be208b41f086
Christian Lawson-Perfect <christianperfect@gmail.com>
2024-07-26 11:03:19 +0100

    amsbsy documentation: add a CTAN link and change "MathJax" to "maths renderer"

--- plasTeX/Packages/amsbsy.py
+++ plasTeX/Packages/amsbsy.py
@@ -1,6 +1,10 @@
-from plasTeX import Command
+"""
+A dummy implementation of the amsbsy package (https://ctan.org/pkg/amsbsy).
 
-# Dummy amsbsy package - handled by mathjax
+The ``\pmb`` and ``\boldsymbol`` commands are handled by the maths renderer.
+"""
+
+from plasTeX import Command
 
 class pmb(Command):
     pass
