936d1461d59e1c8dc7a8c1ec7c38907808ce203c
Christian Lawson-Perfect <christianperfect@gmail.com>
2024-07-26 09:58:16 +0100

    add more explanation to the dummy caption.py

--- plasTeX/Packages/caption.py
+++ plasTeX/Packages/caption.py
@@ -1,1 +1,5 @@
-# Dummy package to prevent plasTeX from loading the LaTeX version.
+"""
+The longtable and caption packages add a command \caption*, which produces a caption with no title or reference number.
+
+This empty file caption.py exists to prevent plasTeX loading the LaTeX caption package when these packages are used.
+"""
