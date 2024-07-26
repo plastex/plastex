710de400e36f4df07fc95701a30824395bed3964
Christian Lawson-Perfect <christianperfect@gmail.com>
2024-07-26 09:55:29 +0100

    add some documentation to framed.py

--- plasTeX/Packages/framed.py
+++ plasTeX/Packages/framed.py
@@ -1,3 +1,19 @@
+"""
+The framed package (https://ctan.org/pkg/framed) provides environments which
+put their contents in a frame.
+
+This adds a plasTeX implementation of the framed environments, and some
+basic styling in the HTML5 renderer.
+
+Only the standard environments ``framed``, ``oframed``, ``shaded``,
+``shaded*``, ``snugshade``, ``snugshade*`` and ``leftbar`` are implemented.
+
+None of the commands listed under "Expert commands" in the framed documentation
+are implemented, and the framed environments don't use the lengths
+``\FrameRule`` or ``\FrameSep``.
+"""
+
+
 from plasTeX import Environment
 
 
